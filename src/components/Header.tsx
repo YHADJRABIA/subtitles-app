bbdb7761f0e9271bcf130a9aeb7a4596a5288f1a
YHR <h_yacine@live.fr>
2024-04-20 01:17:02 +0200

    Implement LanguageToggler

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,19 @@
+import { useTranslations } from 'next-intl'
+import Link from 'next/link'
 import React from 'react'
+import LanguageToggler from './LanguageToggler'
+import styles from './Header.module.scss'
 
 const Header = () => {
-  return <div>Header</div>
+  const t = useTranslations('Navigation')
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <Link href={'/'}>{t('home')}</Link>
+        <LanguageToggler />
+      </nav>
+    </header>
+  )
 }
 
 export default Header
