ed9ec922eca3ca5a4bfde4d798844765d1b68742
YHR <h_yacine@live.fr>
2024-04-24 13:52:50 +0200

    Remove class

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,12 @@ import { useTranslations } from 'next-intl'
 import Link from 'next/link'
 import React from 'react'
 import LanguageToggler from './LanguageToggler'
-import styles from './Header.module.scss'
 
 const Header = () => {
   const t = useTranslations('Navigation')
   return (
-    <header className={styles.header}>
-      <nav className={styles.nav}>
+    <header>
+      <nav>
         <Link href={'/'}>{t('home')}</Link>
         <LanguageToggler />
       </nav>
