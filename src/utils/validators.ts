2efa53c193581bbbd4e2edd6fccd9da3f1baee85
YHR <h_yacine@live.fr>
2024-04-27 00:26:48 +0200

    Add form validation utilities

---
+++ src/utils/validators.ts
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+// Returns true if input is empty or has whitespaces
+export const isEmpty = (input: string): boolean => !input || !input.trim()
+
+export const isValidEmail = (email: string): boolean => {
+  const re =
+    // eslint-disable-next-line
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return re.test(email)
+}
+
+// Google reCAPTCHA validation (validating the frontend's generated token through Google's API)
+export const validateHuman = async (token: string): Promise<boolean> => {
+  const res = await axios.post(
+    `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.GOOGLE_RECAPTCHA_SECRET}&response=${token}`
+  )
+  return res.data.success // True if human user
+}
