80b59c7e182fdbbf6d03f075733b296ee7abe696
YHR <h_yacine@live.fr>
2024-05-02 18:00:57 +0200

    Limit to 2 fonts

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,12 +1,17 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Familjen_Grotesk, Inter } from 'next/font/google'
 import '@/styles/globals.scss'
 import 'react-toastify/dist/ReactToastify.css'
 import { ReactNode } from 'react'
 import Header from '@/components/Layout/Header'
 import Footer from '@/components/Layout/Footer'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'], variable: '--font-body' })
+const familjenGrotesk = Familjen_Grotesk({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-heading',
+})
 
 export const metadata: Metadata = {
   title: 'Stocks app',
@@ -24,7 +29,7 @@ export default function LocaleLayout({
 }: PropTypes) {
   return (
     <html lang={locale}>
-      <body className={inter.className}>
+      <body className={`${inter.variable} ${familjenGrotesk.variable}`}>
         <Header />
         <main>{children}</main>
         <Footer />
