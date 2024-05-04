1b9179f72e8a6de5d34886b588fd76d7c8462e7d
YHR <h_yacine@live.fr>
2024-05-05 00:16:25 +0200

    Update middleware to handle protected routes with internationalisation

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,38 @@
 import createMiddleware from 'next-intl/middleware'
 import { locales } from './utils/locales'
+import withAuth, { NextRequestWithAuth } from 'next-auth/middleware'
+import { NextFetchEvent, NextRequest } from 'next/server'
+import { isPublicPath } from './utils/paths'
 
-export default createMiddleware({
-  // List of supported locales
+const intlMiddleware = createMiddleware({
   locales: locales,
-
-  // Used when no locale matches
   defaultLocale: locales[0],
 })
 
+const authMiddleware = withAuth(
+  function onSuccess(req) {
+    return intlMiddleware(req)
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => token !== null,
+    },
+    pages: {
+      signIn: '/login',
+    },
+  }
+)
+
+// Run authMiddleware for protected routes else run intlMiddleware
+export default function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl
+  const isPublicPage = isPublicPath(pathname)
+
+  return isPublicPage
+    ? intlMiddleware(req)
+    : authMiddleware(req as NextRequestWithAuth, {} as NextFetchEvent)
+}
+
 export const config = {
-  // Match only internationalized pathnames and redirects accordingly
-  matcher: ['/', '/signup', '/login', '/(en|fr)/:path*'],
+  matcher: ['/((?!api|_next|.*\\..*).*)'],
 }
