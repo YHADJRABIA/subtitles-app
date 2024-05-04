4d975ec3f35d38fa9a39ca85372596f18e88cea5
YHR <h_yacine@live.fr>
2024-05-05 01:51:46 +0200

    Rename

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,6 @@
 import { useTranslations } from 'next-intl'
 
-export default function Home() {
+export default function HomePage() {
   const t = useTranslations('Index')
   return <p>{t('title')}</p>
 }
