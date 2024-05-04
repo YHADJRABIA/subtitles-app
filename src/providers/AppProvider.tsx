d340da98474742a9d2c19f0ad0577c7f07874297
YHR <h_yacine@live.fr>
2024-05-04 13:47:41 +0200

    Add session provider

---
+++ src/providers/AppProvider.tsx
@@ -0,0 +1,23 @@
+'use client'
+import store from '@/lib/redux/store'
+import { SessionProvider } from 'next-auth/react'
+import { ReactNode } from 'react'
+import { Provider } from 'react-redux'
+import { ToastContainer } from 'react-toastify'
+
+interface PropTypes {
+  children: ReactNode
+}
+
+const AppProvider = ({ children }: PropTypes) => {
+  /*     <Provider store={store}> 
+      </Provider>  */
+  return (
+    <SessionProvider>
+      <ToastContainer />
+      {children}
+    </SessionProvider>
+  )
+}
+
+export default AppProvider
