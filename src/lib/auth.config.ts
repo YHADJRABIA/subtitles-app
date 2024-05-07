6944c2814350d83f7e3d3bc3da469bf2a36614c0
YHR <h_yacine@live.fr>
2024-05-08 01:52:14 +0200

    Configure next-auth

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -9,6 +9,7 @@ import { isDevelopment } from '@/utils/general'
 import { getUserByEmail } from '@/utils/mongoose'
 import { isValidEmail, isValidPassword } from '@/utils/validators'
 import { getErrorMessage } from '@/utils/errors'
+import { loginSchema } from '@/types/schemas'
 
 const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, NEXTAUTH_SECRET } = process.env
 
@@ -17,17 +18,14 @@ interface PropTypes {
   account: { provider: string }
 }
 
-interface Credentials {
-  email: string
-  password: string
-}
-
 export const authOptions: NextAuthOptions = {
   session: {
     strategy: 'jwt',
   },
+  // Replaces built-in /api/auth/xxx pages with our custom pages
   pages: {
-    signIn: '/login', // Replaces built-in /api/auth/signIn form page with our custom page
+    signIn: '/login',
+    /*     error:'/autherror' */ // TODO: complete later
   },
   secret: NEXTAUTH_SECRET!,
   debug: isDevelopment,
@@ -56,27 +54,30 @@ export const authOptions: NextAuthOptions = {
       // Runs on credential login (with email & password)
       async authorize(credentials) {
         try {
-          const { email, password } = credentials as Credentials
+          const validatedFields = loginSchema.safeParse(credentials)
+          if (validatedFields.success) {
+            const { email, password } = validatedFields.data
 
-          if (!isValidEmail(email) || !isValidPassword(password)) {
-            throw new Error('Missing fields')
+            if (!isValidEmail(email) || !isValidPassword(password)) {
+              throw new Error('Missing fields')
+            }
+            await connectDB()
+            const existingUser = await getUserByEmail(email)
+
+            // If no matching user or user registered via Google (no password)
+            if (!existingUser || !existingUser?.password) {
+              throw new Error("User doesn't exist")
+            }
+
+            const passwordsMatch = await bycrptjs.compare(
+              password,
+              existingUser.password
+            )
+            if (!passwordsMatch) throw new Error('Incorrect password')
+
+            // Passing down user to JWT
+            return existingUser // TODO: Limit what's passed down
           }
-          await connectDB()
-          const existingUser = await getUserByEmail(email)
-
-          // If no matching user or user registered via Google (no password)
-          if (!existingUser || !existingUser?.password) {
-            throw new Error("User doesn't exist")
-          }
-
-          const passwordsMatch = await bycrptjs.compare(
-            password,
-            existingUser.password
-          )
-          if (!passwordsMatch) throw new Error('Incorrect password')
-
-          // Passing down user to JWT
-          return existingUser // TODO: Limit what's passed down
         } catch (err) {
           console.error('Authorization failed:', getErrorMessage(err))
           throw err // Propagate error to frontend
@@ -86,23 +87,41 @@ export const authOptions: NextAuthOptions = {
   ],
 
   callbacks: {
-    // Called after authorize
+    // Called after authorize, user is only populated on login
     jwt({ token, user }) {
-      if (user) {
-        token.user = user
-      }
-      // Passing down token to session
-      return token
+      if (!user) return token // Logged out
+
+      return { ...token, isVerifiedEmail: user.isVerifiedEmail } // Passing down token to session
     },
 
     // Called after jwt
     session({ session, token }) {
-      return { ...session, user: { ...session.user, id: token.sub } }
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: token.sub,
+          isVerifiedEmail: token.isVerifiedEmail,
+        },
+      }
     },
 
-    async signIn({ user, account, profile }: PropTypes) {
+    async signIn({ user, account }: PropTypes) {
+      const [withGoogle, withCredentials] = [
+        account.provider === 'google',
+        account.provider === 'credentials',
+      ]
+
       if (!user) return null
-      if (account.provider === 'google') {
+
+      // Credentials login
+      if (withCredentials) {
+        // Deny access if unverified email
+        if (!user.isVerifiedEmail) return false
+      }
+
+      // Google login
+      if (withGoogle) {
         try {
           const { name, email, image } = user
           await connectDB()
@@ -114,7 +133,7 @@ export const authOptions: NextAuthOptions = {
             name,
             email,
             image,
-            isVerified: true,
+            isVerifiedEmail: true, // Verify email automatically
           })
           const res = await newUser.save()
           if (res.status === 200 || res.status === 201) {
@@ -124,7 +143,7 @@ export const authOptions: NextAuthOptions = {
           console.error('Google SignIn failed:', getErrorMessage(err))
         }
       }
-      return user
+      return true
     },
   },
 }
