185cebcbaf31addb9e28e3401b20c63d799f27fd
YHR <h_yacine@live.fr>
2024-05-08 01:29:50 +0200

    Improve routes readability

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,2 +1,5 @@
-export const publicRoutes = ['/', '/register', '/login']
+export const LOGIN_ROUTE = '/login'
+export const DEFAULT_LOGIN_REDIRECT_ROUTE = '/dashboard'
+export const publicRoutes = ['/', '/register', LOGIN_ROUTE]
 export const protectedRoutes = ['/dashboard']
+export const apiAuthPrefix = '/api/auth'
