4c21b9c39274c24d13b186b5c9d640eb5548f043
YHR <h_yacine@live.fr>
2024-05-08 01:28:37 +0200

    Create helper mongoose function

--- src/utils/mongoose.ts
+++ src/utils/mongoose.ts
@@ -1,3 +1,4 @@
 import { UserModel } from '@/models/user.model'
 
 export const getUserByEmail = (email: string) => UserModel.findOne({ email })
+export const getUserById = (id: string) => UserModel.findById({ _id: id })
