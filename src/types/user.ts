ffcf49f8d583810a2feb9703c805550c6e8d40dc
YHR <h_yacine@live.fr>
2024-05-09 00:07:35 +0200

    Update api type

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,6 +1,10 @@
-export interface UserAPIType {
+import { DefaultSession } from 'next-auth'
+
+export type UserAPIType = {
+  emailVerified: Date
   name: string
   email: string
-  password?: string
   image: string
 }
+
+export type ExtendedUser = DefaultSession['user'] & UserAPIType
