b7aab68be45b192b854ad9cc7f8b9193df6d5dc7
YHR <h_yacine@live.fr>
2024-05-09 00:07:18 +0200

    Generate verificationToken

---
+++ src/lib/auth/token.ts
@@ -0,0 +1,28 @@
+import { VerificationTokenModel } from '@/models/verificationToken.model'
+import {
+  deleteVerificationTokenById,
+  getVerificationTokenByEmail,
+} from '@/utils/db/verification-token'
+import { v4 as uuidv4 } from 'uuid'
+
+export const generateVerificationToken = async (email: string) => {
+  // Generate random token
+  const token = uuidv4()
+  const expires = new Date().getTime() + 1000 * 3600 * 1 // Token expires in 1 hour
+
+  // Check if existing token already sent for this email to delete it
+  const existingToken = await getVerificationTokenByEmail(email)
+
+  if (existingToken) {
+    await deleteVerificationTokenById(existingToken.id)
+  }
+
+  // Create verification token
+  const verificationToken = await VerificationTokenModel.create({
+    email,
+    token,
+    expires: new Date(expires),
+  })
+
+  return verificationToken
+}
