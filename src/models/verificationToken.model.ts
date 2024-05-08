aee4647b654e11e8ceab18471a628c605c5ae24d
YHR <h_yacine@live.fr>
2024-05-09 00:05:05 +0200

    Add verification token model

---
+++ src/models/verificationToken.model.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+const { Schema } = mongoose
+
+const verificationTokenSchema = new Schema({
+  email: {
+    type: String,
+    required: [true, 'Please provide an email'],
+    trim: true,
+    unique: true,
+  },
+  token: {
+    type: String,
+    required: [true, 'Missing token'],
+  },
+  expires: {
+    type: Date,
+    required: [true, 'Missing expiry date'],
+  },
+})
+
+verificationTokenSchema.index({ email: 1, token: 1 }, { unique: true }) // Email-token combination has to be unique
+
+export const VerificationTokenModel =
+  mongoose.models.VerificationToken ||
+  mongoose.model('VerificationToken', verificationTokenSchema)
