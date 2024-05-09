5dea4c63a165e25f3f3fd991ad54dc680e9ce6af
YHR <h_yacine@live.fr>
2024-05-10 01:36:02 +0200

    Update type

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,10 +1,10 @@
 import { DefaultSession } from 'next-auth'
 
 export type UserAPIType = {
-  emailVerified: Date
-  name: string
   email: string
-  image: string
+  emailVerified?: Date
+  name?: string
+  image?: string
 }
 
 export type ExtendedUser = DefaultSession['user'] & UserAPIType
