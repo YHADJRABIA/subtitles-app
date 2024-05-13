7749bb1381df80a47a0b6131cf5c5e726cfc9177
YHR <h_yacine@live.fr>
2024-05-14 01:29:29 +0200

    Update function's documentation + create other utility

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -1,9 +1,10 @@
 import { v4 as uuidv4 } from 'uuid'
+import bcryptjs from 'bcryptjs'
 
 /**
  * Generates a random UUID token with an expiration date.
- * @param {number} [validHours=1] - Number of hours until expiration. Defaults to 1 hour.
- * @returns {{ token: string, expirationDate: number }} - The generated token and its expiration date.
+ * @param {number} [validHours=1] Number of hours until expiration. Defaults to 1 hour.
+ * @returns {{ token: string, expirationDate: number }} The generated token and its expiration date.
  */
 
 export const generateUUIDToken = (
@@ -18,4 +19,21 @@ export const generateUUIDToken = (
     token,
     expirationDate,
   }
+}
+
+/**
+ * Generates a salted hash for the given password.
+ * @param password Password to hash.
+ * @param rounds Number of rounds to use for salting. Defaults to 10.
+ * @returns A Promise resolving to the hashed password.
+ */
+
+export const hashPassword = async (
+  password: string,
+  rounds: number = 10
+): Promise<string> => {
+  const salt = await bcryptjs.genSalt(rounds)
+  const hashedPassword = await bcryptjs.hash(password, salt)
+
+  return hashedPassword
 }
