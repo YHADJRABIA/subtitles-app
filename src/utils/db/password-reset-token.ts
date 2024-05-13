bbce033d94bf8e363415845f35ecc6f4c709aa3a
YHR <h_yacine@live.fr>
2024-05-13 12:28:27 +0200

    Add password reset utils

---
+++ src/utils/db/password-reset-token.ts
@@ -0,0 +1,28 @@
+import { PasswordResetTokenModel } from '@/models/passwordResetToken.model'
+import { getErrorMessage } from '../errors'
+
+export const getPasswordResetTokenByToken = async (token: string) => {
+  try {
+    const existingToken = await PasswordResetTokenModel.findOne({ token })
+    return existingToken
+  } catch (err) {
+    console.error('Get password reset token by token', getErrorMessage(err))
+  }
+}
+
+export const getPasswordResetTokenByEmail = async (email: string) => {
+  try {
+    const existingToken = await PasswordResetTokenModel.findOne({ email })
+    return existingToken
+  } catch (err) {
+    console.error('Get password reset token by email', getErrorMessage(err))
+  }
+}
+
+export const deletePasswordResetTokenById = async (id: string) => {
+  try {
+    await PasswordResetTokenModel.deleteOne({ _id: id })
+  } catch (err) {
+    console.error('Delete password reset token by id:', getErrorMessage(err))
+  }
+}
