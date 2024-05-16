abdb9a3eeba07b33c67df330f5d2ee283fc4f98c
YHR <h_yacine@live.fr>
2024-05-16 21:56:41 +0200

    Add register page

---
+++ src/app/[locale]/(auth)/register/page.tsx
@@ -0,0 +1,14 @@
+import RegisterForm from '@/components/Auth/RegisterForm'
+import React from 'react'
+
+const RegisterPage = /* async */ () => {
+  /*   const session = await getServerSession() // TODO: PUT back after refactor to server component
+
+  if (session) {
+    redirect('/')
+  } */
+
+  return <RegisterForm />
+}
+
+export default RegisterPage
