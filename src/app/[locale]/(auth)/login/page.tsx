fa7b5c339a3f77748da65ca81e656ff979e20e4c
YHR <h_yacine@live.fr>
2024-05-18 00:02:32 +0200

    Make login page server component and refactor

---
+++ src/app/[locale]/(auth)/login/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import LoginForm from '@/components/Auth/LoginForm'
+
+const LoginPage = () => {
+  /*   const session = await getServerSession() // TODO: PUT back after refactor to server component
+
+  if (session) {
+    redirect('/dashboard')
+  } */
+
+  return <LoginForm />
+}
+
+export default LoginPage
