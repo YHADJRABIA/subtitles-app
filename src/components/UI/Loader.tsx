e1a0f516ca6b5022ab0b4e2ecd5b259dd70c09c6
YHR <h_yacine@live.fr>
2024-05-22 16:52:13 +0200

    Add optional ClassName prop

--- src/components/UI/Loader.tsx
+++ src/components/UI/Loader.tsx
@@ -1,13 +1,21 @@
 import { ImSpinner2 } from 'react-icons/im'
 import styles from './Loader.module.scss'
+import cn from 'classnames'
 
 interface PropTypes {
   size?: number
+  className?: string
 }
 
 const Loader = (props: PropTypes) => {
-  const { size } = props
-  return <ImSpinner2 {...props} size={size ?? 16} className={styles.root} />
+  const { size, className } = props
+  return (
+    <ImSpinner2
+      {...props}
+      size={size ?? 16}
+      className={cn(styles.root, className)}
+    />
+  )
 }
 
 export default Loader
