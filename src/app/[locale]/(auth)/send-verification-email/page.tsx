9f17c56127cc0d091e1b44c326032dcb78958f74
YHR <h_yacine@live.fr>
2024-05-23 19:18:29 +0200

    Add Email verification form page

---
+++ src/app/[locale]/(auth)/send-verification-email/page.tsx
@@ -0,0 +1,8 @@
+import SendVerificationEmailForm from '@/components/Auth/SendVerificationEmailForm'
+import React from 'react'
+
+const SendVerificationEmailPage = () => {
+  return <SendVerificationEmailForm />
+}
+
+export default SendVerificationEmailPage
