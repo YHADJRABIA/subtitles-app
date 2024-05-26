28576e480e1398d7a472b3c89c0994b7ae2b55d4
YHR <h_yacine@live.fr>
2024-05-27 00:01:22 +0200

    Add password recovery page

---
+++ src/app/[locale]/(auth)/password/recovery/page.tsx
@@ -0,0 +1,8 @@
+import PasswordRecoveryForm from '@/components/Auth/Password/PasswordRecoveryForm'
+import React from 'react'
+
+const PasswordRecoveryPage = () => {
+  return <PasswordRecoveryForm />
+}
+
+export default PasswordRecoveryPage
