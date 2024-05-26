08dfb859b564399cd6e66ec977b101532d51e2b6
YHR <h_yacine@live.fr>
2024-05-27 00:01:03 +0200

    Update mail url

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -63,7 +63,7 @@ export const sendPasswordResetEmail = async (
   email: string,
   token: string
 ) => {
-  const resetLink = `${websiteUrl}/reset-password?token=${token}`
+  const resetLink = `${websiteUrl}/password/reset?token=${token}`
 
   const outgoingEmail = {
     title: 'Reset your password',
