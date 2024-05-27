a7d05f888a031a0e2a7a854fdffc5e97a3e8c78c
YHR <h_yacine@live.fr>
2024-05-27 17:11:11 +0200

    Relocate page with simpler route

---
+++ src/app/[locale]/(auth)/password/reset/page.tsx
@@ -0,0 +1,8 @@
+import PasswordResetForm from '@/components/Auth/Password/PasswordResetForm'
+import React from 'react'
+
+const PasswordResetPage = () => {
+  return <PasswordResetForm />
+}
+
+export default PasswordResetPage
