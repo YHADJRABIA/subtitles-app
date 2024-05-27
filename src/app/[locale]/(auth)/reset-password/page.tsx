6b679ef5b5ffa48dd213f9e99d8f545a9bdc55e5
YHR <h_yacine@live.fr>
2024-05-27 17:11:23 +0200

    Remove unused page (replaced)

--- src/app/[locale]/(auth)/reset-password/page.tsx
+++
@@ -1,8 +0,0 @@
-import ResetPasswordForm from '@/components/Auth/ResetPasswordForm'
-import React from 'react'
-
-const ResetPasswordPage = () => {
-  return <ResetPasswordForm />
-}
-
-export default ResetPasswordPage
