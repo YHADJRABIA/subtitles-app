4b0cc631dc3e91429ef38d29168282affb01d081
YHR <h_yacine@live.fr>
2024-05-29 00:01:54 +0200

    Create hook

---
+++ src/hooks/useDelocalisePathname.ts
@@ -0,0 +1,18 @@
+import { locales } from '@/lib/i18n/navigation'
+import { usePathname } from 'next/navigation'
+
+/**
+ * Custom hook to extract pathname without locale prefix.
+ *
+ * @returns {string} Pathname without locale prefix.
+ */
+
+export const useDelocalisePathname = () => {
+  const pathname = usePathname()
+
+  const localePattern = locales.join('|')
+  const re = new RegExp(`^/(${localePattern})(?=/|$)`)
+
+  // Remove the locale prefix
+  return pathname.replace(re, '')
+}
