42783152a8ff7b1590136a9cba78790dfb363c51
YHR <h_yacine@live.fr>
2024-05-31 00:18:51 +0200

    Add corresponding hook to reduce logic in view

---
+++ src/hooks/useChangeLanguage.ts
@@ -0,0 +1,51 @@
+import { useState, useTransition } from 'react'
+import { useRouter } from 'next/navigation'
+
+import { useLocale } from 'next-intl'
+import { getLanguageByLocaleValue } from '@/utils/language'
+import { useDelocalisedPathname } from './useDelocalisePathname'
+import { Locale } from '@/types/locale'
+
+/**
+ * Provides current language with corresponding flag & label.
+ * Allows to switch into language of provided locale by redirecting to matching route.
+ * Provides pending state of language change and opening state of language menu.
+ */
+
+const useChangeLanguage = () => {
+  const router = useRouter()
+  const unlocalisedPathname = useDelocalisedPathname()
+  const currentLocale = useLocale()
+
+  const [isPending, startTransition] = useTransition()
+  const [isOpen, setIsOpen] = useState(false)
+  const [currentLanguage, setCurrentLanguage] = useState(
+    getLanguageByLocaleValue(currentLocale as Locale)
+  )
+
+  const handleClose = () => setIsOpen(false)
+
+  const onChangeLanguage = (locale: Locale) => {
+    if (locale === currentLocale) return handleClose()
+
+    const selectedLanguage = getLanguageByLocaleValue(locale)
+
+    if (selectedLanguage) {
+      startTransition(() => {
+        setCurrentLanguage(selectedLanguage)
+        router.replace(`/${locale}/${unlocalisedPathname}`)
+      })
+    }
+  }
+
+  return {
+    isPending,
+    isOpen,
+    setIsOpen,
+    currentLanguage,
+    handleClose,
+    onChangeLanguage,
+  }
+}
+
+export default useChangeLanguage
