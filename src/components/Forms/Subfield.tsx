28762f0102f5bb0264d20ff3376b793f9d5137e0
YHR <h_yacine@live.fr>
2024-06-01 00:34:20 +0200

    Refactor Subfield as separate component

---
+++ src/components/Forms/Subfield.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import styles from './Subfield.module.scss'
+import { MdErrorOutline as ErrorIcon } from 'react-icons/md'
+import cn from 'classnames'
+
+interface PropTypes {
+  label?: string
+  hasIcon?: boolean
+  isShown: boolean
+}
+
+const Subfield = ({ label, hasIcon, isShown }: PropTypes) => {
+  return (
+    <div
+      className={cn(styles.root, 'hidden', {
+        visible: isShown,
+      })}
+    >
+      {hasIcon && <ErrorIcon size={14} className={styles.errorIcon} />}
+      <small
+        style={{
+          color: hasIcon ? 'var(--primary-red-color)' : undefined,
+        }}
+      >
+        {label}
+      </small>
+    </div>
+  )
+}
+
+export default Subfield
