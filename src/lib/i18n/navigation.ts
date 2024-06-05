f182b39e32bdaf5adf8081c033ea3d5e16cb5e24
YHR <h_yacine@live.fr>
2024-06-06 00:21:29 +0200

    Translate some routes

--- src/lib/i18n/navigation.ts
+++ src/lib/i18n/navigation.ts
@@ -8,15 +8,33 @@ export const locales = ['en', 'fr']
 export const localePrefix = 'as-needed' // No prefix for the default locale. /en -> /
 
 export const pathnames = {
-  // If all locales use the same pathname, a
-  // single external path can be provided.
+  // If all locales use the same pathname, a single external path can be provided
   '/': '/',
 
-  // If locales use different paths, you can
-  // specify each external path per locale.
+  // If locales use different paths, you can specify each external path per locale
+  '/dashboard': {
+    en: '/dashboard',
+    fr: '/tableau-de-bord',
+  },
   '/register': {
     en: '/register',
     fr: '/inscription',
+  },
+  '/login': {
+    en: '/login',
+    fr: '/connexion',
+  },
+  '/send-verification-email': {
+    en: '/send-verification-email',
+    fr: '/envoi-email-verification',
+  },
+  '/verify-email': {
+    en: '/verify-email',
+    fr: '/verification-email',
+  },
+  '/password/recovery': {
+    en: '/password/recovery',
+    fr: '/mot-de-passe/reinitialisation',
   },
 } satisfies Pathnames<typeof locales>
 
