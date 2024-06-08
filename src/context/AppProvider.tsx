c8d59c1cf45950c31bf91edc285c8806a7365cd5
YHR <h_yacine@live.fr>
2024-06-08 21:47:39 +0200

    Enable client components to access translations

--- src/context/AppProvider.tsx
+++ src/context/AppProvider.tsx
@@ -2,6 +2,8 @@ import store from '@/lib/redux/store'
 
 import { ReactNode } from 'react'
 import { Provider } from 'react-redux'
+import { NextIntlClientProvider } from 'next-intl'
+import { getMessages } from 'next-intl/server'
 import { ToastContainer } from 'react-toastify'
 import AuthProvider from './AuthProvider'
 
@@ -9,14 +11,18 @@ interface PropTypes {
   children: ReactNode
 }
 
-const AppProvider = ({ children }: PropTypes) => {
+const AppProvider = async ({ children }: PropTypes) => {
+  const messages = await getMessages()
+
   /*     <Provider store={store}> 
       </Provider>  */
   return (
-    <AuthProvider>
-      <ToastContainer />
-      {children}
-    </AuthProvider>
+    <NextIntlClientProvider messages={messages}>
+      <AuthProvider>
+        <ToastContainer />
+        {children}
+      </AuthProvider>
+    </NextIntlClientProvider>
   )
 }
 
