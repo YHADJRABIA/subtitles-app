1e8167bb880fa5abe212442d08d25c0d8131126a
YHR <h_yacine@live.fr>
2024-06-12 00:03:20 +0200

    Remove boilerplate & fix UX

--- src/components/Auth/VerifyEmail/index.tsx
+++ src/components/Auth/VerifyEmail/index.tsx
@@ -2,7 +2,6 @@
 import { getErrorMessage } from '@/utils/errors'
 import axios from 'axios'
 import { useSearchParams } from 'next/navigation'
-import { useEffect, useState, useCallback } from 'react'
 import styles from './VerifyEmail.module.scss'
 import Typography from '../../UI/Typography'
 
@@ -13,50 +12,68 @@ import {
 import Link from 'next/link'
 import Loading from './loading'
 import { useTranslations } from 'next-intl'
+import useInfo from '@/hooks/useInfo'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import {
+  EmailVerificationSchema,
+  EmailVerificationValidator,
+} from '@/types/schemas/auth'
+import { useIsomorphicLayoutEffect } from '@/hooks/useIsomorphicLayoutEffect'
 
 const VerifyEmail = () => {
-  const t = useTranslations('Auth.VerifyEmail')
+  const t = {
+    verifyEmail: useTranslations('Auth.VerifyEmail'),
+    zod: useTranslations('Zod'),
+  }
 
-  const [isLoading, setIsLoading] = useState(true)
   const searchParams = useSearchParams()
-  const token = searchParams.get('token')
+  const token = searchParams.get('token') ?? ''
 
-  const [info, setInfo] = useState({ label: '', status: '' })
+  const { info, setInfoMessage } = useInfo()
 
-  const handleValidate = useCallback(async () => {
+  const handleValidate: SubmitHandler<EmailVerificationSchema> = async user => {
     try {
-      const res = await axios.post('/api/users/verify-token', { token })
-      setInfo({ label: res.data.message, status: 'success' })
+      const res = await axios.post('/api/users/verify-token', user)
+      setInfoMessage(res.data.message, 'success')
     } catch (err) {
-      setInfo({
-        label: getErrorMessage(err?.response.data.message),
-        status: 'error',
-      })
-    } finally {
-      setIsLoading(false)
+      setInfoMessage(
+        getErrorMessage(err?.response.data.message) ?? getErrorMessage(err),
+        'error'
+      )
     }
+  }
+
+  const {
+    handleSubmit,
+    setValue,
+    formState: { isSubmitted },
+  } = useForm<EmailVerificationSchema>({
+    resolver: zodResolver(EmailVerificationValidator(t.zod)),
+    delayError: 400,
+    mode: 'onChange',
+  })
+
+  // Set token's value programmatically since no existing field
+  useIsomorphicLayoutEffect(() => {
+    setValue('token', token)
   }, [token])
 
-  useEffect(() => {
-    if (!token) {
-      setIsLoading(false)
-      return setInfo({ label: 'No token provided', status: 'error' })
-    }
+  useIsomorphicLayoutEffect(() => {
+    if (!token) return setInfoMessage(t.zod('token.missing'), 'error')
+    handleSubmit(handleValidate)()
   }, [])
 
-  useEffect(() => {
-    token && handleValidate()
-  }, [])
-
-  const isError = info.status === 'error'
+  const isError = info.type === 'error'
 
   const Icon = isError ? ErrorIcon : SuccessIcon
 
+  const showLoader = !(isSubmitted || isError)
   return (
     <div className={styles.root}>
       <div className={styles.loadingSection}>
-        {isLoading ? (
-          <Loading label={t('loading')} />
+        {showLoader ? (
+          <Loading label={t.verifyEmail('loading')} />
         ) : (
           <>
             <Icon
@@ -71,11 +88,18 @@ const VerifyEmail = () => {
           </>
         )}
       </div>
-      {isError && (
+      {isSubmitted && (
         <Typography className={styles.cta}>
-          {t('issue')}{' '}
-          <Link href="/send-verification-email">{t('resend_email')}</Link>
-          {/* TODO: Send automatically to associated user if expired token for UX */}
+          {isError ? (
+            <>
+              {t.verifyEmail('issue')}{' '}
+              <Link href="/send-verification-email">
+                {t.verifyEmail('resend_email')}
+              </Link>
+            </>
+          ) : (
+            <Link href="/login">{t.verifyEmail('fallback')}</Link>
+          )}
         </Typography>
       )}
     </div>
@@ -83,3 +107,7 @@ const VerifyEmail = () => {
 }
 
 export default VerifyEmail
+
+{
+  /* TODO: Send automatically to associated user if expired token for UX */
+}
