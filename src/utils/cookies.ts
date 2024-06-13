813ecff53b3b43e8ef672163919dd14dd06d2be6
YHR <h_yacine@live.fr>
2024-06-13 13:29:52 +0200

    Get locale cookie from header cookies or req + refactor

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -1,15 +1,26 @@
 import { defaultLocale } from '@/lib/i18n/navigation'
+import { RequestCookies } from 'next/dist/compiled/@edge-runtime/cookies'
+import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies'
 import { NextRequest } from 'next/server'
 
 /**
- * Retrieves the locale from the request cookie or defaults to the defaultLocale.
- * @param req The NextRequest object representing the incoming request.
+ * Extracts the locale from the NEXT_LOCALE cookie out of a Next cookies object.
+ * @param cookies The cookies object containing the locale cookie.
  * @returns A string representing the locale.
  */
-
-export const getLocaleFromRequestCookie = (req: NextRequest) => {
-  const nextLocaleCookie = req.cookies.get('NEXT_LOCALE')
-  const locale = nextLocaleCookie ? nextLocaleCookie.value : defaultLocale
+const getLocaleFromNextCookie = (
+  cookies: ReadonlyRequestCookies | RequestCookies
+) => {
+  const nextCookie = cookies.get('NEXT_LOCALE')
+  const locale = nextCookie ? nextCookie.value : defaultLocale
 
   return locale
+}
+
+export const getLocaleFromNextRequest = (req: NextRequest) => {
+  return getLocaleFromNextCookie(req.cookies)
+}
+
+export const getLocaleFromNextCookies = (cookies: ReadonlyRequestCookies) => {
+  return getLocaleFromNextCookie(cookies)
 }
