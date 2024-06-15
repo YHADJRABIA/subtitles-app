ff215f1d034e51d50349e99077b7c373b835c52e
YHR <h_yacine@live.fr>
2024-06-15 02:00:42 +0200

    Locate auth actions in separate file

--- src/lib/auth/actions.ts
+++ src/lib/auth/actions.ts
@@ -1,5 +1,18 @@
+import { AuthFormData } from '@/types/schemas/auth'
+import axios from 'axios'
 import { signIn } from 'next-auth/react'
 
-export const handleGoogleLogin = () => {
-  signIn('google', { callbackUrl: '/dashboard' })
+export const handleGoogleLogin = async () => {
+  await signIn('google', { callbackUrl: '/dashboard' })
+}
+
+export const handleCredentialsLogin = async (user: AuthFormData) => {
+  await signIn('credentials', {
+    ...user,
+    redirect: false,
+  })
+}
+
+export const handleRegister = async (user: AuthFormData) => {
+  await axios.post('/api/users/register', user)
 }
