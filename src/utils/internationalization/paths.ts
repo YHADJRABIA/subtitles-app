a6242c4905adfaa050ca0be5e08566d6dc4b0795
YHR <h_yacine@live.fr>
2024-06-18 00:16:11 +0200

    Add utility function for href sanitisation

---
+++ src/utils/internationalization/paths.ts
@@ -0,0 +1,16 @@
+import { locales } from '@/lib/i18n/navigation'
+
+/**
+ * Strips locale prefix from a given localized pathname.
+ * Example: '/fr/test' becomes '/test'
+ * @param {string} localisedPathname Pathname might include a locale prefix
+ * @returns Pathname without locale prefix
+ */
+
+export const removeLocalePrefixFromPathname = (localisedPathname: string) => {
+  const localePattern = locales.join('|')
+  const re = new RegExp(`^/(${localePattern})(?=/|$)`)
+  const sanitisedPathname = localisedPathname.replace(re, '')
+
+  return sanitisedPathname
+}
