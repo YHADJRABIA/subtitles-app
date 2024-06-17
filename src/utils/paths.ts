a5294d36fa056f44576b75e73c79d4ba29a06219
YHR <h_yacine@live.fr>
2024-06-18 00:02:46 +0200

    Add utility function to sanitise paths

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -10,3 +10,23 @@ export const isPublicPath = (pathname: string): boolean => {
   )
   return publicPathnameRegex.test(pathname)
 }
+
+interface HrefParams {
+  searchParams: string
+  pathname: string
+}
+
+/**
+ * Sanitises input href by returning searchParams or clean pathname.
+ * @param {string | URL} href The URL or string representation of a URL to sanitise.
+ * @returns {HrefParams} Object containing search parameters (`searchParams`) and pathname (`pathname`).
+ */
+
+export const extractHrefParams = (href: string | URL): HrefParams => {
+  const url =
+    typeof href === 'string' ? new URL(href, 'http://example.com') : href
+  return {
+    searchParams: url.search,
+    pathname: url.pathname,
+  }
+}
