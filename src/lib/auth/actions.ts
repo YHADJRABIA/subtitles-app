7d157190c40eee51263dda71b748b62197bb33ba
YHR <h_yacine@live.fr>
2024-06-21 00:43:47 +0200

    Externalise Auth actions

--- src/lib/auth/actions.ts
+++ src/lib/auth/actions.ts
@@ -1,6 +1,8 @@
 import {
   AccountLoginSchema,
   AccountRegistrationSchema,
+  EmailVerificationSchema,
+  PasswordRecoverySchema,
   SendEmailVerificationSchema,
 } from '@/types/schemas/auth'
 import axios from 'axios'
@@ -15,7 +17,8 @@ export const handleGoogleLogin = async () => {
 export const handleCredentialsLogin = async (user: AccountLoginSchema) => {
   return await signIn('credentials', {
     ...user,
-    redirect: false,
+    redirect: true,
+    callbackUrl: '/dashboard',
   })
 }
 
@@ -27,4 +30,16 @@ export const handleSendVerificationEmail = async (
   user: SendEmailVerificationSchema
 ) => {
   return await axios.post('/api/users/send-verification-email', user)
+}
+
+export const handleVerifyEmailValidationToken = async (
+  user: EmailVerificationSchema
+) => {
+  return await axios.post('/api/users/verify-token', user)
+}
+
+export const handleSendPasswordRecoveryEmail = async (
+  user: PasswordRecoverySchema
+) => {
+  return await axios.post('/api/users/password/recover', user)
 }
