d1479acd0df30b95a5a3004f672363ce3c90a0fc
YHR <h_yacine@live.fr>
2024-06-22 00:46:53 +0200

    Reference remaining routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,15 @@
 export const LOGIN_ROUTE = '/login'
 export const DEFAULT_LOGIN_REDIRECT_ROUTE = '/dashboard'
-export const publicRoutes = ['/', '/verify-email', '/register', LOGIN_ROUTE]
+
+// These routes don't need authenticaition
+export const publicRoutes = [
+  '/',
+  '/verify-email',
+  '/send-verification-email',
+  '/password/recovery',
+  '/password/reset',
+  '/register',
+  LOGIN_ROUTE,
+]
 export const protectedRoutes = ['/dashboard']
 export const apiAuthPrefix = '/api/auth'
