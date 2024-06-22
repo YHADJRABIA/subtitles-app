7994f611c091d40f27be77063639f091440a08d6
YHR <h_yacine@live.fr>
2024-06-22 02:08:28 +0200

    Add homepage's layout

---
+++ src/app/[locale]/(main)/layout.tsx
@@ -0,0 +1,28 @@
+import React, { ReactNode } from 'react'
+import { getTranslations } from 'next-intl/server'
+import Header from '@/components/Layout/Header'
+import Footer from '@/components/Layout/Footer'
+
+export const generateMetadata = async () => {
+  const t = await getTranslations({ namespace: 'Metadata.Homepage' })
+
+  return {
+    title: t('title'),
+    description: t('description'),
+  }
+}
+
+interface PropTypes {
+  children: ReactNode
+}
+const layout = ({ children }: PropTypes) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
+}
+
+export default layout
