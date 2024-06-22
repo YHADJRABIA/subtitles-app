f1f6ee905aa6bbe8823b62323c28ac9b66d9b8c6
YHR <h_yacine@live.fr>
2024-06-23 00:04:05 +0200

    Create  HeroBanner file

---
+++ src/components/UI/HeroBanner.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface PropTypes {
+  title: string
+  description: string
+  image?: string
+}
+
+const HeroBanner = ({ title, description }: PropTypes) => {
+  return <div>HeroBanner</div>
+}
+
+export default HeroBanner
