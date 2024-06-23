ce8410b087e4fed57b840a3383929a7650a85106
YHR <h_yacine@live.fr>
2024-06-24 00:40:36 +0200

    Add utility functions for middleware

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -1,14 +1,55 @@
-import { locales } from '@/lib/i18n/navigation'
-import { publicRoutes } from '@/routes/routes'
+import { locales, pathnames } from '@/lib/i18n/navigation'
+import { REGISTER_ROUTE, LOGIN_ROUTE, publicRoutes } from '@/routes/routes'
+import { Pathname } from '@/types/pathnames'
 
-export const isPublicPath = (pathname: string): boolean => {
+/**
+ * Checks if a pathname matches any of the localised routes.
+ * @param {string[]} routes Array of routes to check.
+ * @param {string} pathname Pathname to match.
+ * @returns {boolean} True if the pathname matches any of the localised routes, false otherwise.
+ */
+export const pathnameMatchesLocalisedRoutes = (
+  routes: string[],
+  pathname: Pathname
+): boolean => {
+  // missing getLocalisedPathsForPathname
   const publicPathnameRegex = RegExp(
-    `^(/(${locales.join('|')}))?(${publicRoutes
+    `^(/(${locales.join('|')}))?(${routes
       .flatMap(p => (p === '/' ? ['', '/'] : p))
       .join('|')})/?$`,
     'i'
   )
   return publicPathnameRegex.test(pathname)
+}
+
+// Returns true if pathname or its localised version is included in publicRoutes
+export const isPublicPath = (pathname: Pathname): boolean => {
+  const localisedPublicRoutes = publicRoutes.flatMap(route =>
+    getLocalisedPathsForPathname(route)
+  )
+  return pathnameMatchesLocalisedRoutes(localisedPublicRoutes, pathname)
+}
+
+// Returns true if pathname is /login or /register or a localised version of it
+export const isLoginOrRegisterPath = (pathname: Pathname): boolean => {
+  const loginRegisterRoutes = [
+    ...getLocalisedPathsForPathname(LOGIN_ROUTE),
+    ...getLocalisedPathsForPathname(REGISTER_ROUTE),
+  ]
+  return pathnameMatchesLocalisedRoutes(loginRegisterRoutes, pathname)
+}
+
+/**
+ * Retrieves localised paths for a given pathname.
+ * @param {Pathname} path Pathname to retrieve localised paths for.
+ * @returns {string[]} Array of localised paths corresponding to the given pathname.
+ */
+export const getLocalisedPathsForPathname = (path: Pathname): string[] => {
+  const localisedPaths = pathnames[path]
+  if (typeof localisedPaths === 'object') {
+    return Object.values(localisedPaths)
+  }
+  return [localisedPaths]
 }
 
 interface HrefParams {
