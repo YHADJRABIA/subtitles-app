56c8496aaa79c5ca985e423ee5ed88cdfea14838
YHR <h_yacine@live.fr>
2024-06-24 00:39:39 +0200

    Put routes in variables and enforce typing

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,8 @@
-export const LOGIN_ROUTE = '/login'
-export const DEFAULT_LOGIN_REDIRECT_ROUTE = '/dashboard'
+import { Pathname } from '@/types/pathnames'
+
+export const LOGIN_ROUTE: Pathname = '/login'
+export const REGISTER_ROUTE: Pathname = '/register'
+export const DEFAULT_LOGIN_REDIRECT_ROUTE: Pathname = '/dashboard'
 
 // These routes don't need authenticaition
 export const publicRoutes = [
@@ -8,8 +11,9 @@ export const publicRoutes = [
   '/send-verification-email',
   '/password/recovery',
   '/password/reset',
-  '/register',
+  REGISTER_ROUTE,
   LOGIN_ROUTE,
-]
-export const protectedRoutes = ['/dashboard']
+] satisfies Pathname[]
+
+export const protectedRoutes = ['/dashboard'] satisfies Pathname[]
 export const apiAuthPrefix = '/api/auth'
