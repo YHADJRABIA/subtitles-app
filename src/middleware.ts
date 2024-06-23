8cc6c2f999a474560ce56af22e5d722fd1b8f46a
YHR <h_yacine@live.fr>
2024-06-24 00:42:01 +0200

    Redirect user according to authentication state and to target route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import createMiddleware from 'next-intl/middleware'
 import withAuth, { NextRequestWithAuth } from 'next-auth/middleware'
-import { NextFetchEvent } from 'next/server'
-import { isPublicPath } from './utils/paths'
+import { NextFetchEvent, NextResponse } from 'next/server'
+import { isLoginOrRegisterPath, isPublicPath } from './utils/paths'
 import {
   defaultLocale,
   localePrefix,
@@ -9,6 +9,8 @@ import {
   pathnames,
 } from './lib/i18n/navigation'
 import { LOGIN_ROUTE } from './routes/routes'
+import { getToken } from 'next-auth/jwt'
+import { Pathname } from './types/pathnames'
 
 const intlMiddleware = createMiddleware({
   defaultLocale,
@@ -17,33 +19,48 @@ const intlMiddleware = createMiddleware({
   pathnames,
 })
 
+// AuthMiddleware is skipped if user hits public routes
 const authMiddleware = withAuth(
   function onSuccess(req) {
+    // Skip to IntlMiddleware if user is authenticated
     return intlMiddleware(req)
   },
   {
     callbacks: {
-      authorized: ({ token }) => token !== null, // If there's a token, user is authenticated
+      authorized: ({ token }) => token !== null, // User is considered authenticated if token isn't null
     },
     pages: {
-      // Replace Next-auth's built-in pages with ours
-      signIn: LOGIN_ROUTE,
+      // Replace Next-auth's built-in pages with own custom pagess
+      signIn: LOGIN_ROUTE, // Unauthenticated user is redirected here when attempting to access protected routes
       error: '/error',
     },
   }
 )
 
-// Run authMiddleware for protected routes else run intlMiddleware
-export default function middleware(req: NextRequestWithAuth) {
+// Called whenever request matches config
+// Dispatches request to middlewares depending on conditions
+export default async function middleware(req: NextRequestWithAuth) {
   const { pathname } = req.nextUrl
+  const isPublicRoute = isPublicPath(pathname as Pathname)
 
-  const isPublicRoute = isPublicPath(pathname)
+  const session = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET, // Secret needed to access session outside of authMiddleware (has to be the same as in authOptions at /auth.config.ts)
+  })
 
+  const isConnected = !!session
+
+  // Prevent authenticated user from reaching login/register pages by redirecting to homepage
+  if (isConnected && isLoginOrRegisterPath(pathname as Pathname)) {
+    return NextResponse.redirect(new URL('/', req.url))
+  }
+
+  // Skip authMiddleware if route is public
   return isPublicRoute
     ? intlMiddleware(req)
     : authMiddleware(req, {} as NextFetchEvent)
 }
 
 export const config = {
-  matcher: ['/((?!api|_next|.*\\..*).*)'],
+  matcher: ['/((?!api|_next|.*\\..*).*)'], // Middleware triggers whenever route matches this regex
 }
