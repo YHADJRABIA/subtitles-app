bd3ed33d59a399bafb4e3a7edf94bbb7bdec28da
YHR <h_yacine@live.fr>
2024-06-26 00:01:45 +0200

    Refactor

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,16 +4,22 @@ export const LOGIN_ROUTE: Pathname = '/login'
 export const REGISTER_ROUTE: Pathname = '/register'
 export const DEFAULT_LOGIN_REDIRECT_ROUTE: Pathname = '/dashboard'
 
-// These routes don't need authenticaition
+/** Routes accessible without requiring authentication. */
 export const publicRoutes = [
   '/',
   '/verify-email',
   '/send-verification-email',
   '/password/recovery',
   '/password/reset',
+] satisfies Pathname[]
+
+/** Routes accessible only as unauthenticated user */
+export const loginRegisterRoutes = [
   REGISTER_ROUTE,
   LOGIN_ROUTE,
 ] satisfies Pathname[]
 
+/** Routes redirecting to '/login' if accessed as unauthenticated user. */
 export const protectedRoutes = ['/dashboard'] satisfies Pathname[]
-export const apiAuthPrefix = '/api/auth'
+
+export const apiAuthPrefix = '/api/auth' // Routes handled by Next-Auth
