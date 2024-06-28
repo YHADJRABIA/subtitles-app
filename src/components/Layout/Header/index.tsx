2963debe634ba6d7ade55518bcc812d30712010e
YHR <h_yacine@live.fr>
2024-06-29 00:03:34 +0200

    Complete Header

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -2,11 +2,19 @@ import React from 'react'
 import LanguageMenu from '../LanguageMenu'
 import styles from './Header.module.scss'
 import Nav from './Nav'
+import AuthSection from './AuthSection'
+import Logo from './Logo'
 
-const Header = () => {
+interface PropTypes {
+  isConnected: boolean
+}
+
+const Header = ({ isConnected }: PropTypes) => {
   return (
     <header className={styles.root}>
+      <Logo />
       <Nav className={styles.nav} />
+      <AuthSection showAccount={isConnected} />
       <LanguageMenu className={styles.languageMenu} />
     </header>
   )
