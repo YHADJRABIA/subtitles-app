564262b55d10cfcadd08b5b96a4bfd58b4a0b58a
YHR <h_yacine@live.fr>
2024-06-29 00:02:38 +0200

    Update main layout and pass auth session

--- src/app/[locale]/(main)/layout.tsx
+++ src/app/[locale]/(main)/layout.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode } from 'react'
 import { getTranslations } from 'next-intl/server'
 import Header from '@/components/Layout/Header/index'
 import Footer from '@/components/Layout/Footer'
+import { getUserSession } from '@/utils/session'
 
 export const generateMetadata = async () => {
   const t = await getTranslations({ namespace: 'Metadata.Homepage' })
@@ -15,10 +16,11 @@ export const generateMetadata = async () => {
 interface PropTypes {
   children: ReactNode
 }
-const layout = ({ children }: PropTypes) => {
+const layout = async ({ children }: PropTypes) => {
+  const user = await getUserSession()
   return (
     <>
-      <Header />
+      <Header isConnected={!!user} />
       <main>{children}</main>
       <Footer />
     </>
