b9b2308cadbda91e8811370c20ce167eb48f4fc5
YHR <h_yacine@live.fr>
2024-07-03 00:31:15 +0200

    Add isOnMobile hook

---
+++ src/hooks/useIsOnMobile.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react'
+
+import { useEventListener } from './useEventListener'
+
+const useIsOnMobile = (): boolean => {
+  const [isCompact, setIsCompact] = useState(true)
+
+  const updateMatchQuery = () =>
+    setIsCompact(matchMedia('(max-width: 767px)').matches)
+
+  useEffect(() => updateMatchQuery(), [])
+  useEventListener('resize', updateMatchQuery)
+
+  return isCompact
+}
+
+export default useIsOnMobile
