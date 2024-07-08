30b844532d47fd79b03491b711b679bb13f2d51e
YHR <h_yacine@live.fr>
2024-07-09 00:58:26 +0200

    Add hook

---
+++ src/hooks/useNavLinks.ts
@@ -0,0 +1,69 @@
+import { useTranslations } from 'next-intl'
+
+import {
+  GrHomeRounded as HomeIcon,
+  GrCircleQuestion as AboutIcon,
+  GrUserSettings as DashboardIcon,
+  GrLogout as LogoutIcon,
+  GrKey as LoginIcon,
+  GrUserExpert as RegisterIcon,
+} from 'react-icons/gr'
+
+import { LuSubtitles as SubtitlesIcon } from 'react-icons/lu'
+
+interface PropTypes {
+  isConnected: boolean
+}
+
+/**
+ * Returns nav links to show based on authentication status
+ **/
+const useNavLinks = ({ isConnected }: PropTypes) => {
+  const t = useTranslations('Navigation')
+  const navLinks = [
+    {
+      url: '/',
+      label: t('home'),
+      icon: HomeIcon,
+    },
+    {
+      url: '/about',
+      label: t('about'),
+      icon: AboutIcon,
+    },
+    {
+      url: '/subtitles',
+      label: t('subtitles'),
+      icon: SubtitlesIcon,
+    },
+    ...(isConnected
+      ? [
+          {
+            url: '/dashboard',
+            label: t('dashboard'),
+            icon: DashboardIcon,
+          },
+          {
+            url: '/logout',
+            label: t('logout'),
+            icon: LogoutIcon,
+          },
+        ]
+      : [
+          {
+            url: '/register',
+            label: t('register'),
+            icon: RegisterIcon,
+          },
+          {
+            url: '/login',
+            label: t('login'),
+            icon: LoginIcon,
+          },
+        ]),
+  ].filter(Boolean)
+
+  return navLinks
+}
+
+export default useNavLinks
