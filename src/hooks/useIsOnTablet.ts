bb06953dcffe8c541e6d7cde937be5280f46fe8c
YHR <h_yacine@live.fr>
2024-07-14 02:30:40 +0200

    Add hook

---
+++ src/hooks/useIsOnTablet.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react'
+
+import { useEventListener } from './useEventListener'
+
+const useIsOnTablet = (): boolean => {
+  const [isOnTablet, setIsOnTablet] = useState(true)
+
+  const updateMatchQuery = () =>
+    setIsOnTablet(matchMedia('(max-width: 768px)').matches)
+
+  useEffect(() => updateMatchQuery(), [])
+  useEventListener('resize', updateMatchQuery)
+
+  return isOnTablet
+}
+
+export default useIsOnTablet
