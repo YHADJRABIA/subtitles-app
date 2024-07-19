34f714760d608f83282a71f00482c33a4112b156
YHR <h_yacine@live.fr>
2024-07-19 02:51:31 +0200

    Integrate dashboard nav

---
+++ src/app/[locale]/(protected)/_components/Navbar/index.tsx
@@ -0,0 +1,39 @@
+'use client'
+import React from 'react'
+import styles from './Navbar.module.scss'
+import {
+  LuUserSquare2 as OverviewIcon,
+  LuSettings as SettingsIcon,
+} from 'react-icons/lu'
+import { useTranslations } from 'next-intl'
+import NavLink from './NavLink'
+import { usePathname } from '@/lib/i18n/navigation'
+
+const Navbar = () => {
+  const currentPath = usePathname()
+  const t = useTranslations('Dashboard')
+  const links = [
+    {
+      url: '/dashboard',
+      label: t('overview'),
+      icon: OverviewIcon,
+    },
+    {
+      url: '/dashboard/settings',
+      label: t('settings'),
+      icon: SettingsIcon,
+    },
+  ]
+  return (
+    <nav className={styles.root}>
+      <ul>
+        {links.map((link, id) => {
+          const isActive = link?.url === currentPath
+          return <NavLink key={id} link={link} isActive={isActive} />
+        })}
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
