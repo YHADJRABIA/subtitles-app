30a15103eabe49770a53c4e8d63df993bc2f84c3
YHR <h_yacine@live.fr>
2024-07-20 03:39:42 +0200

    Fix typescript

--- src/utils/session.ts
+++ src/utils/session.ts
@@ -1,9 +1,9 @@
 import { authOptions } from '@/lib/auth/auth.config'
-import { getServerSession } from 'next-auth'
+import { getServerSession, User } from 'next-auth'
 
 // Returns current user if authenticated
-export const getUserSession = async () => {
+export const getUserSession = async (): Promise<User> => {
   const authUserSession = await getServerSession(authOptions)
 
-  return authUserSession?.user
+  return authUserSession?.user as User
 }
