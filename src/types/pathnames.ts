6a2b01cbbfd789cd7d5c06a768dfd75da16b1dcf
YHR <h_yacine@live.fr>
2024-07-23 00:07:13 +0200

    Fix deprecation

--- src/types/pathnames.ts
+++ src/types/pathnames.ts
@@ -1,5 +1,5 @@
 import { pathnames } from '@/lib/i18n/navigation'
-import { Pathnames } from 'next-intl/navigation'
+import { Pathnames } from 'next-intl/routing'
 import { Locales } from './locale'
 
 export type Pathname = keyof typeof pathnames
