c60b1f43d9c036eea00d3e6805f595f381683bed
YHR <h_yacine@live.fr>
2024-07-25 01:50:19 +0200

    Add fileds to User type

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,17 +1,24 @@
 import { DefaultSession } from 'next-auth'
+import { Locale } from './locale'
 
 export type UserType = {
   email: string
   isVerifiedEmail: boolean
+  lastUpdateDate: Date
   creationDate: Date
   lastLoginDate?: Date
+  favoriteLocale: Locale
+  isTwoFactorEnabled: boolean
 }
 
 export type UserAPIType = DefaultSession['user'] & {
   email: string
   emailVerified?: Date
   createdAt: Date
+  updatedAt?: Date
   lastLogin?: Date
+  favoriteLocale: Locale
+  isTwoFactorEnabled: boolean
 }
 
 export type ExtendedUser = DefaultSession['user'] & UserType
