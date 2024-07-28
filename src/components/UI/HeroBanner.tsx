be91c88d9d5572e75ce5b4ca64780131ab9bf388
YHR <h_yacine@live.fr>
2024-07-29 00:25:11 +0200

    Create HeroBanner

--- src/components/UI/HeroBanner.tsx
+++ src/components/UI/HeroBanner.tsx
@@ -1,13 +1,27 @@
 import React from 'react'
+import styles from './HeroBanner.module.scss'
+
+import cn from 'classnames'
+import Typography from './Typography'
 
 interface PropTypes {
   title: string
   description: string
-  image?: string
+  image?: string // TODO: update later according to design
+  className?: string
 }
 
-const HeroBanner = ({ title, description }: PropTypes) => {
-  return <div>HeroBanner</div>
+const HeroBanner = ({ className, title, description }: PropTypes) => {
+  return (
+    <section className={cn(styles.root, className)}>
+      <Typography tag="h1" weight="bold" className={styles.title}>
+        {title}
+      </Typography>
+      <Typography className={styles.description} size="l">
+        {description}
+      </Typography>
+    </section>
+  )
 }
 
 export default HeroBanner
