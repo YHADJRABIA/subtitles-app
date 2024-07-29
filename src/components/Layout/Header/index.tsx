817ce478967ac8fab87b902286c5d7bb5d372106
YHR <h_yacine@live.fr>
2024-07-30 00:19:54 +0200

    Add className prop to header

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 import styles from './Header.module.scss'
 import Nav from './Nav'
 import Logo from './Logo'
+import cn from 'classnames'
 
 interface PropTypes {
   isConnected: boolean
+  className?: string
 }
 
-const Header = ({ isConnected }: PropTypes) => {
+const Header = ({ isConnected, className }: PropTypes) => {
   return (
-    <header className={styles.root}>
+    <header className={cn(styles.root, className)}>
       <Logo isInvertedColor size={50} />
       <Nav className={styles.nav} isConnected={isConnected} />
     </header>
