d62cb1f934a352f59a05e8c09b87b9d10d36c306
YHR <h_yacine@live.fr>
2024-07-30 00:17:45 +0200

    Add props to HeroBanner

--- src/components/UI/HeroBanner.tsx
+++ src/components/UI/HeroBanner.tsx
@@ -7,17 +7,23 @@ import Typography from './Typography'
 interface PropTypes {
   title: string
   description: string
+  align?: 'left' | 'center' | 'right'
   image?: string // TODO: update later according to design
   className?: string
 }
 
-const HeroBanner = ({ className, title, description }: PropTypes) => {
+const HeroBanner = ({
+  className,
+  title,
+  align = 'center',
+  description,
+}: PropTypes) => {
   return (
     <section className={cn(styles.root, className)}>
-      <Typography tag="h1" weight="bold" className={styles.title}>
+      <Typography tag="h1" weight="bold" align={align} className={styles.title}>
         {title}
       </Typography>
-      <Typography className={styles.description} size="l">
+      <Typography size="l" align={align} className={styles.description}>
         {description}
       </Typography>
     </section>
