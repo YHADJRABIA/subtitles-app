616d38293cb1e02798bfd6341a47c0ab66f04601
YHR <h_yacine@live.fr>
2024-07-29 02:13:06 +0200

    Fix type

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,10 +1,11 @@
 import { notFound } from 'next/navigation'
 import { getRequestConfig } from 'next-intl/server'
 import { locales } from './lib/i18n/navigation'
+import { Locale } from './types/locale'
 
 export default getRequestConfig(async ({ locale }) => {
   // Validate incoming `locale` param
-  if (!locales.includes(locale as string)) notFound() // Returns not-found page if no match
+  if (!locales.includes(locale as Locale)) notFound() // Returns not-found page if no match
 
   return {
     messages: (await import(`../messages/${locale}.json`)).default,
