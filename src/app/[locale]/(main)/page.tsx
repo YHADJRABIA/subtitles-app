28649b73df45843451847ccc88922efe5abecfd3
YHR <h_yacine@live.fr>
2024-08-01 00:39:07 +0200

    Integrate homepage

--- src/app/[locale]/(main)/page.tsx
+++ src/app/[locale]/(main)/page.tsx
@@ -1,7 +1,23 @@
-import Typography from '@/components/UI/Typography'
+import HeroBanner from '@/components/UI/HeroBanner'
+import styles from './page.module.scss'
 import { useTranslations } from 'next-intl'
+import CtaSection from './_components/CtaSection'
+
+const MAIN_ASSET_PATH = '/assets/pellicules.svg'
 
 export default function HomePage() {
   const t = useTranslations('Index')
-  return <Typography>Test</Typography>
+
+  return (
+    <div className={styles.root}>
+      <HeroBanner
+        image={MAIN_ASSET_PATH}
+        imageAlt={t('HeroBanner.image_alt')}
+        title={t('HeroBanner.heading')}
+        description={t('HeroBanner.description')}
+        ctaElements={<CtaSection />}
+        className={styles.heroBanner}
+      />
+    </div>
+  )
 }
