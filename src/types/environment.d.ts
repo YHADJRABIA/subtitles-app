ad18f52b0d0c3024eca14ffc4dc9bc0bd39aec27
YHR <h_yacine@live.fr>
2024-07-31 16:04:55 +0200

    Type env variables

---
+++ src/types/environment.d.ts
@@ -0,0 +1,16 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      MONGODB_URI: string
+      JWT_SECRET: string
+      JWT_EXPIRATION_TIME: string
+      GOOGLE_CLIENT_ID: string
+      GOOGLE_CLIENT_SECRET: string
+      NEXTAUTH_SECRET: string
+      NODEMAILER_SERVICE: string
+      NODEMAILER_SENDER_EMAIL: string
+      SUPPORT_LINK: string
+      NODE_ENV: 'development' | 'production'
+    }
+  }
+}
