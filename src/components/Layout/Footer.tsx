bff1d7ba862a6ad443a9c4f364783ca8b0c0b65f
YHR <h_yacine@live.fr>
2024-08-01 00:14:07 +0200

    Refactor footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,20 +1,20 @@
 import { PORTFOLIO_LINK } from '@/utils/general'
 import { getCurrentYear } from '@/utils/date'
 import { useTranslations } from 'next-intl'
-import styles from './Footer.module.scss'
 import React from 'react'
-import { Link } from '@/lib/i18n/navigation'
+import Typography from '../UI/Typography'
 
 const Footer = () => {
   const t = useTranslations('Footer')
   return (
     <footer>
-      <small>
-        &copy;{getCurrentYear()} –{' '}
-        <Link href={PORTFOLIO_LINK} target="_blank" className={styles.link}>
-          {`${t('first_name')} ${t('last_name')}`}
-        </Link>
-      </small>
+      <Typography
+        tag="small"
+        link={{ href: PORTFOLIO_LINK, openInNewTab: true }}
+        size="xs"
+      >
+        &copy;{getCurrentYear()} – {t('name')}
+      </Typography>
     </footer>
   )
 }
