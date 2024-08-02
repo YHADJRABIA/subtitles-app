fedec879eba8a91d564bf946c8a39b1831f291a3
YHR <h_yacine@live.fr>
2024-08-03 00:42:40 +0200

    Add TODO

--- src/app/[locale]/(main)/page.tsx
+++ src/app/[locale]/(main)/page.tsx
@@ -8,6 +8,7 @@ import { Metadata } from 'next'
 
 const MAIN_ASSET_PATH = '/assets/film-rolls.svg'
 
+// TODO: Complete SEO fields
 export const generateMetadata = async (): Promise<Metadata> => {
   const locale = getNextLocale()
   const t = await getTranslations({ locale, namespace: 'Metadata' })
