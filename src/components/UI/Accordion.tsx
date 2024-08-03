ee17f699c3cdd6eeb0095916be30bfd4a84ca810
YHR <h_yacine@live.fr>
2024-08-03 03:03:10 +0200

    Add accordion component

---
+++ src/components/UI/Accordion.tsx
@@ -0,0 +1,93 @@
+'use client'
+import React, { useRef, useState } from 'react'
+import styles from './Accordion.module.scss'
+import { PiCaretDownBold as CaretIcon } from 'react-icons/pi'
+
+import cn from 'classnames'
+import Typography from './Typography'
+
+type ItemType = { title: string; body: string }
+
+interface PropTypes {
+  items: ItemType[]
+  className?: string
+  expandMultiple?: boolean
+}
+
+const Accordion = ({ className, items, expandMultiple = true }: PropTypes) => {
+  // Index of opened item(s)
+  const [openStates, setOpenStates] = useState<boolean[]>(
+    items.map(() => false)
+  )
+
+  const handleToggle = (index: number) => {
+    setOpenStates(prevStates =>
+      expandMultiple
+        ? prevStates.map((isOpen, idx) => (idx === index ? !isOpen : isOpen))
+        : prevStates.map((_, idx) => idx === index)
+    )
+  }
+
+  return (
+    <ul className={cn(styles.root, className)}>
+      {items.map((item, idx) => (
+        <AccordionItem
+          key={idx}
+          {...item}
+          isOpen={openStates[idx]}
+          onToggle={() => handleToggle(idx)}
+        />
+      ))}
+    </ul>
+  )
+}
+
+export default Accordion
+
+interface AccordionItemTypes extends ItemType {
+  isOpen: boolean
+  onToggle: () => void
+}
+
+const AccordionItem = ({
+  title,
+  body,
+  isOpen,
+  onToggle,
+}: AccordionItemTypes) => {
+  const contentHeight = useRef<HTMLDivElement | null>(null)
+
+  return (
+    <li className={styles.item}>
+      <span
+        className={cn(styles.titleContainer, { [styles.isExpanded]: isOpen })}
+        onClick={onToggle}
+      >
+        <Typography
+          tag="h2"
+          size="m"
+          weight="bold"
+          className={styles.title}
+          align="left"
+        >
+          {title}
+        </Typography>
+        <CaretIcon
+          size={16}
+          className={cn(styles.toggler, { verticalFlip: isOpen })}
+        />
+      </span>
+      <div
+        ref={contentHeight}
+        className={cn(styles.collapseable, { [styles.open]: isOpen })}
+        style={{
+          maxHeight: isOpen ? `${contentHeight.current?.scrollHeight}px` : '0',
+        }}
+      >
+        <Typography tag="h3" align="left" className={styles.description}>
+          {body}
+        </Typography>
+      </div>
+    </li>
+  )
+}
