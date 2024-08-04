05133a5f8b95c754b6e7ffb079e526e070ac8916
YHR <h_yacine@live.fr>
2024-08-05 01:43:04 +0200

    Add responsive utility function

---
+++ src/utils/responsiveCN.ts
@@ -0,0 +1,64 @@
+import cn from 'classnames'
+import { capitaliseFirstLetter } from './string'
+
+export type ResponsiveValues<T> = T | [T | undefined, T, T?]
+
+export type Breakpoint = 'Small' | 'Medium' | 'Large'
+
+export const BREAKPOINT_NAMES: Breakpoint[] = ['Small', 'Medium', 'Large']
+
+type CSSModule = {
+  [key: string]: string
+}
+
+function buildClassName<T>(
+  prefix: string,
+  value: T,
+  breakpointName?: Breakpoint
+): string {
+  let className = `${prefix}`
+
+  if (typeof value === `string`) {
+    className += capitaliseFirstLetter(value)
+  }
+  if (typeof value === `number`) {
+    className += value.toString()
+  }
+  if (typeof breakpointName !== `undefined`) {
+    className += breakpointName
+  }
+  return className
+}
+
+// Use-case: 0 is a value
+const shouldEnableClassName = <T>(value: T): boolean => {
+  if (value === 0) return true
+
+  return !!value
+}
+
+export const responsiveCN =
+  (css: CSSModule) =>
+  <T>(classNamePrefix: string, value?: ResponsiveValues<T>): string => {
+    let classNames: string
+
+    if (Array.isArray(value)) {
+      classNames = cn(
+        BREAKPOINT_NAMES.map((breakpointName, index) => {
+          const className = buildClassName(
+            classNamePrefix,
+            value[index],
+            breakpointName
+          )
+          return { [css[className]]: shouldEnableClassName(value[index]) }
+        })
+      )
+    } else {
+      const className = buildClassName(classNamePrefix, value)
+      classNames = cn({
+        [css[className]]: shouldEnableClassName(value),
+      })
+    }
+
+    return classNames
+  }
