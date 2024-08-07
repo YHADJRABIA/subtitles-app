e1e6db071a2bbbc47ea137a35394e7d1e7e62028
YHR <h_yacine@live.fr>
2024-08-07 02:40:32 +0200

    Add TextWithIcon component

---
+++ src/components/UI/ItemsList/TextWithIcon/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import styles from './TextWithIcon.module.scss'
+import cn from 'classnames'
+import Typography from '../../Typography'
+import { IconType } from 'react-icons/lib'
+
+export interface TextWithIconProps {
+  icon: IconType
+  title: string
+  description: string
+  className?: string
+}
+
+const TextWithIcon = ({
+  className,
+  title,
+  description,
+  icon: Icon,
+}: TextWithIconProps) => {
+  return (
+    <div className={cn(styles.root, className)}>
+      <span className={styles.icon}>
+        <Icon size={20} />
+      </span>
+
+      <div className={styles.textContainer}>
+        <Typography
+          tag="h3"
+          size="m"
+          weight="bold"
+          align="left"
+          className={styles.title}
+        >
+          {title}
+        </Typography>
+
+        <Typography
+          size="s"
+          weight="semiLight"
+          align="left"
+          className={styles.description}
+        >
+          {description}
+        </Typography>
+      </div>
+    </div>
+  )
+}
+
+export default TextWithIcon
