d96e54f0cbb0f4d0a8fc4ce472995f4afe81eecf
YHR <h_yacine@live.fr>
2024-08-09 01:19:25 +0200

    Target body

--- src/hooks/useScrollDirection.ts
+++ src/hooks/useScrollDirection.ts
@@ -1,12 +1,13 @@
 import { useState, useRef } from 'react'
 import { useEventListener } from './useEventListener'
+import { isClient } from '@/utils/general'
 
 export const useScrollDirection = () => {
   const [scrollDirection, setScrollDirection] = useState<'up' | 'down'>('up')
   const lastScrollTop = useRef(0) // UseRef to track last scroll position
 
   const handleScroll = () => {
-    const scrollTop = window.scrollY || document.documentElement.scrollTop
+    const scrollTop = window.scrollY || document.body.scrollTop
 
     if (scrollTop > lastScrollTop.current) {
       // Scrolling down
@@ -19,7 +20,8 @@ export const useScrollDirection = () => {
     lastScrollTop.current = scrollTop <= 0 ? 0 : scrollTop // Update last scroll position
   }
 
-  useEventListener('scroll', handleScroll)
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  if (isClient) useEventListener('scroll', handleScroll, document.body) // TODO: debounce this
 
   return scrollDirection
 }
