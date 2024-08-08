8ddf6df2b9d45be44979f85e9815be1a9659dc29
YHR <h_yacine@live.fr>
2024-08-09 01:42:20 +0200

    Update hard-code name

--- src/utils/hardCode.ts
+++ src/utils/hardCode.ts
@@ -1,2 +1,2 @@
 export const SUPPORT_LINK = 'https://buymeacoffee.com/y.h.r'
-export const PATRUL_TRAILER = 'https://www.youtube.com/watch?v=nNmZNZQ20v8'
+export const PATRUL_TRAILER_LINK = 'https://www.youtube.com/watch?v=nNmZNZQ20v8'
