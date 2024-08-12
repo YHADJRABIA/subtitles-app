bea754152cfdd913b62448356cf89a68f8bd9e86
YHR <h_yacine@live.fr>
2024-08-12 15:42:02 +0200

    Add utility function to split array

---
+++ src/utils/array.ts
@@ -0,0 +1,9 @@
+/**
+ * Joins the elements of a string array into a single string, separated by ", ".
+ * Exemple: ['First', 'Second', 'Third'] => 'First, Second, Third'
+ * @param {string[]} arr Array of strings to be joined.
+ * @returns {string} String with each array element separated by ", ".
+ */
+export const joinWithComma = (arr: string[]): string => {
+  return arr.join(', ')
+}
