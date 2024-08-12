5c3e192f4a2488a2409fc775c7ef74f80e36cc2a
YHR <h_yacine@live.fr>
2024-08-12 03:53:56 +0200

    Remove props

--- src/app/[locale]/(main)/subtitles/[...slug]/_components/SeriesInfo.tsx
+++ src/app/[locale]/(main)/subtitles/[...slug]/_components/SeriesInfo.tsx
@@ -4,17 +4,12 @@ import styles from './SeriesInfo.module.scss'
 import { useTranslations } from 'next-intl'
 import SeriesInfoField from './SeriesInfoField'
 
-type Country = 'Kazakhstan' | 'Russia'
-type Language = 'Russian' | 'Kazakh'
 type NumberOfSeasons = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8'
 type Genre = 'Comedy'
 
 interface PropTypes {
-  country: Country
-  releaseYear: string
   genre: Genre
   NumberOfSeasons: NumberOfSeasons
-  language: Language
   directors: string
   actors: string
   className?: string
@@ -22,20 +17,14 @@ interface PropTypes {
 
 const SeriesInfo = ({
   className,
-  country,
-  releaseYear,
   genre,
   NumberOfSeasons,
-  language,
   directors,
   actors,
 }: PropTypes) => {
   const t = useTranslations('Series')
   return (
     <section className={cn(styles.root, className)}>
-      <SeriesInfoField label={t('country')} value={country} />
-      <SeriesInfoField label={t('language')} value={language} />
-      <SeriesInfoField label={t('release_year')} value={releaseYear} />
       <SeriesInfoField label={t('genre')} value={genre} />
       <SeriesInfoField label={t('number_of_seasons')} value={NumberOfSeasons} />
       <SeriesInfoField label={t('actors')} value={actors} />
