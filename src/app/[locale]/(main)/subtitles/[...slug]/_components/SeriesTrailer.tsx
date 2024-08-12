1f8d0293d5eb04990e143803a626ffd6f318f41c
YHR <h_yacine@live.fr>
2024-08-12 20:30:02 +0200

    Integrate trailer component

---
+++ src/app/[locale]/(main)/subtitles/[...slug]/_components/SeriesTrailer.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import cn from 'classnames'
+import styles from './SeriesTrailer.module.scss'
+import Video from '@/components/Video'
+import Typography from '@/components/UI/Typography'
+import { useTranslations } from 'next-intl'
+
+interface PropTypes {
+  className?: string
+  src: string
+  captionsSrc: string
+  thumbnail?: string
+}
+
+const SeriesTrailer = ({
+  src,
+  captionsSrc,
+  thumbnail,
+  className,
+}: PropTypes) => {
+  const t = useTranslations('Series')
+  if (!src.length || !captionsSrc.length) return
+  return (
+    <div className={cn(styles.root, className)}>
+      <Typography
+        tag="h3"
+        weight="semiBold"
+        align="left"
+        size="s"
+        className={styles.title}
+      >
+        {t('trailer')}
+      </Typography>
+      <Video
+        videoSrc={src}
+        captionsSrc={captionsSrc}
+        thumbnailSrc={thumbnail}
+      />
+    </div>
+  )
+}
+
+export default SeriesTrailer
