49fd4fe38ee15f3bf04e32b86fff5831adf99200
YHR <h_yacine@live.fr>
2024-08-12 17:47:50 +0200

    Revamp SeriesInfo

--- src/app/[locale]/(main)/subtitles/[...slug]/_components/SeriesInfo.tsx
+++ src/app/[locale]/(main)/subtitles/[...slug]/_components/SeriesInfo.tsx
@@ -3,32 +3,38 @@ import cn from 'classnames'
 import styles from './SeriesInfo.module.scss'
 import { useTranslations } from 'next-intl'
 import SeriesInfoField from './SeriesInfoField'
+import { joinWithComma } from '@/utils/array'
 
-type NumberOfSeasons = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8'
-type Genre = 'Comedy'
+type NumberOfSeasons = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
 
 interface PropTypes {
-  genre: Genre
-  NumberOfSeasons: NumberOfSeasons
-  directors: string
-  actors: string
+  directors: string[]
+  actors: string[]
+  numberOfSeaons: NumberOfSeasons
   className?: string
 }
 
 const SeriesInfo = ({
   className,
-  genre,
-  NumberOfSeasons,
   directors,
   actors,
+  numberOfSeaons,
 }: PropTypes) => {
   const t = useTranslations('Series')
   return (
     <section className={cn(styles.root, className)}>
-      <SeriesInfoField label={t('genre')} value={genre} />
-      <SeriesInfoField label={t('number_of_seasons')} value={NumberOfSeasons} />
-      <SeriesInfoField label={t('actors')} value={actors} />
-      <SeriesInfoField label={t('director')} value={directors} />
+      <SeriesInfoField
+        label={t('Seasons.title')}
+        value={String(numberOfSeaons)}
+      />
+      <SeriesInfoField
+        label={t('actors', { count: actors.length })}
+        value={joinWithComma(actors)}
+      />
+      <SeriesInfoField
+        label={t('director', { count: directors.length })}
+        value={joinWithComma(directors)}
+      />
     </section>
   )
 }
