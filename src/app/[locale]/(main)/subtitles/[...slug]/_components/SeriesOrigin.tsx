95e7f0a5c04f5947477cf54a2c6dc9138a04b977
YHR <h_yacine@live.fr>
2024-08-12 03:53:42 +0200

    Add title

--- src/app/[locale]/(main)/subtitles/[...slug]/_components/SeriesOrigin.tsx
+++ src/app/[locale]/(main)/subtitles/[...slug]/_components/SeriesOrigin.tsx
@@ -8,6 +8,7 @@ import {
 } from 'react-icons/pi'
 import Typography from '@/components/UI/Typography'
 import { IconType } from 'react-icons/lib'
+import { useTranslations } from 'next-intl'
 
 type Country = 'Kazakhstan' | 'Russia'
 type Language = 'Russian' | 'Kazakh'
@@ -25,22 +26,35 @@ const SeriesOrigin = ({
   releaseYear,
   className,
 }: PropTypes) => {
+  const t = useTranslations('Series')
   return (
     <div className={cn(styles.root, className)}>
       <div className={styles.top}>
-        <Field icon={CountryIcon} value={country} />
-        <Field icon={YearIcon} value={String(releaseYear)} />
+        <Field icon={CountryIcon} value={country} title={t('country')} />
+        <Field
+          icon={YearIcon}
+          value={String(releaseYear)}
+          title={t('release_year')}
+        />
       </div>
-      <Field icon={LanguageIcon} value={language} />
+      <Field icon={LanguageIcon} value={language} title={t('language')} />
     </div>
   )
 }
 
 export default SeriesOrigin
 
-const Field = ({ icon: Icon, value }: { icon: IconType; value: string }) => (
+const Field = ({
+  icon: Icon,
+  value,
+  title,
+}: {
+  icon: IconType
+  value: string
+  title: string
+}) => (
   <span className={styles.field}>
-    <Icon className={styles.icon} size={22} />
+    <Icon className={styles.icon} size={22} title={title} />
     <Typography size="xs" weight="semiBold">
       {value}
     </Typography>
