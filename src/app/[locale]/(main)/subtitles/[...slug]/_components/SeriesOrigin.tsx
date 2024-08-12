2632abbe6c56b4a70eb8a79e22a8fccc9606216c
YHR <h_yacine@live.fr>
2024-08-12 21:30:23 +0200

    Update SeriesOrigin

--- src/app/[locale]/(main)/subtitles/[...slug]/_components/SeriesOrigin.tsx
+++ src/app/[locale]/(main)/subtitles/[...slug]/_components/SeriesOrigin.tsx
@@ -6,21 +6,25 @@ import {
   PiGlobeHemisphereEastThin as CountryIcon,
   PiCalendarDotsThin as YearIcon,
   PiTagThin as GenreIcon,
+  PiTelevisionThin as SeasonsIcon,
 } from 'react-icons/pi'
+
 import Typography from '@/components/UI/Typography'
 import { IconType } from 'react-icons/lib'
 import { useTranslations } from 'next-intl'
 
 /* TODO: relocate to types folder */
-type Country = 'kz' | 'ru'
-type Language = 'ru' | 'kz'
-type Genre = 'comedy'
+export type SeriesCountry = 'ru' | 'kz'
+export type SeriesLanguage = 'ru' | 'kz'
+export type SeriesGenre = 'comedy'
+export type SeriesNumberOfSeasons = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
 
 interface PropTypes {
-  country: Country
-  language: Language
+  country: SeriesCountry
+  language: SeriesLanguage
   releaseYear: number
-  genre: Genre
+  genre: SeriesGenre
+  numberOfSeasons: SeriesNumberOfSeasons
   className?: string
 }
 
@@ -29,6 +33,7 @@ const SeriesOrigin = ({
   language,
   releaseYear,
   genre,
+  numberOfSeasons,
   className,
 }: PropTypes) => {
   const t = useTranslations('Series')
@@ -41,21 +46,29 @@ const SeriesOrigin = ({
           title={t('Country.title')}
         />
         <Field
+          icon={LanguageIcon}
+          value={t(`Language.${language}`)}
+          title={t('Language.title')}
+        />
+      </div>
+
+      <div className={styles.row}>
+        <Field
           icon={YearIcon}
           value={String(releaseYear)}
           title={t('release_year')}
         />
+        <Field
+          icon={GenreIcon}
+          value={t(`Genre.${genre}`)}
+          title={t('Genre.title')}
+        />
       </div>
 
       <Field
-        icon={LanguageIcon}
-        value={t(`Language.${language}`)}
-        title={t('Language.title')}
-      />
-      <Field
-        icon={GenreIcon}
-        value={t(`Genre.${genre}`)}
-        title={t('Genre.title')}
+        icon={SeasonsIcon}
+        value={t('Seasons.number_of_seasons', { count: numberOfSeasons })}
+        title={t('Seasons.title')}
       />
     </div>
   )
