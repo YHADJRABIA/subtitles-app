31c1d0bc27ec7c6f407eb9f5435be195e75092fb
YHR <h_yacine@live.fr>
2024-08-14 04:44:46 +0200

    Add EpisodesModal variation

---
+++ src/components/Modals/EpisodesModal/index.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import Typography from '@/components/UI/Typography'
+
+import styles from './EpisodesModal.module.scss'
+import { downloadFile } from '@/utils/download'
+import { SeriesEpisode } from '@/app/[locale]/(main)/series/[...slug]/_components/SeriesSubtitles' // TODO: relocate to types folder
+import Modal from '../Modal'
+import { useTranslations } from 'next-intl'
+
+interface EpisodesModalProps {
+  isOpen: boolean
+  onClose: () => void
+  title?: string
+  seasonNumber: number
+  episodes: SeriesEpisode[]
+}
+
+const EpisodesModal = ({
+  isOpen,
+  onClose,
+  title,
+  seasonNumber,
+  episodes,
+}: EpisodesModalProps) => {
+  const t = useTranslations('Series')
+
+  const handleDownload = (subtitleUrl: string, episodeNumber: number) => {
+    const filename = `s${String(seasonNumber).padStart(2, '0')}-ep${String(episodeNumber).padStart(2, '0')}.srt`
+    downloadFile(subtitleUrl, filename)
+  }
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} title={title}>
+      <ul className={styles.root}>
+        {episodes.map(({ episode, subtitleUrl }) => (
+          <li key={episode} className={styles.item}>
+            <Typography
+              align="left"
+              size="xs"
+              weight="semiBold"
+              className={styles.label}
+            >
+              {t('Subtitles.episode', { count: episode })}
+            </Typography>
+
+            <Typography
+              onClick={() => handleDownload(subtitleUrl, episode)}
+              size="xs"
+              className={styles.cta}
+            >
+              {t('Subtitles.download')}
+            </Typography>
+          </li>
+        ))}
+      </ul>
+    </Modal>
+  )
+}
+
+export default EpisodesModal
