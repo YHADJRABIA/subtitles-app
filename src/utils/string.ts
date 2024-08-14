db114c352a65db84831fd28fa7eaaeb5b2dd2e74
YHR <h_yacine@live.fr>
2024-08-14 03:38:00 +0200

    Remove util function

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,12 +1,3 @@
 export const capitaliseFirstLetter = (string: string) => {
   return string ? string[0].toUpperCase() + string.slice(1) : string
 }
-
-/**
- * Formats a number with a leading zero if it's less than 10.
- * @param num Number to format.
- * @returns Formatted string.
- */
-export const formatNumber = (num: number): string => {
-  return num < 10 ? `0${num}` : String(num)
-}
