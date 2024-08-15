fca6e0da810907e792d7e6f5c0c4d9ce95b07cf2
YHR <h_yacine@live.fr>
2024-08-15 03:23:51 +0200

    Add editableAvatar WIP

---
+++ src/app/[locale]/(protected)/dashboard/_components/EditableAvatar/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import cn from 'classnames'
+import styles from './EditableAvatar.module.scss'
+import Avatar from '@/components/Avatar/Avatar'
+import { MdCameraAlt as CameraIcon } from 'react-icons/md'
+import { useTranslations } from 'next-intl'
+
+interface PropTypes {
+  src?: string | null
+  className?: string
+  onEditAvatar: () => void
+}
+
+// TODO: finish up
+const EditableAvatar = ({ className, src, onEditAvatar }: PropTypes) => {
+  const t = useTranslations('EditableAvatar')
+  return (
+    <div className={cn(styles.root, className)}>
+      <Avatar src={src} className={styles.avatar} />
+      <CameraIcon
+        size={20}
+        className={styles.ctaIcon}
+        onClick={onEditAvatar}
+        title={t('edit')}
+      />
+    </div>
+  )
+}
+
+export default EditableAvatar
