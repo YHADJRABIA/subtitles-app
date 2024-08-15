7dd03fc975ba65d2fbe3d9d0b30afda2b64bda4c
YHR <h_yacine@live.fr>
2024-08-15 03:15:23 +0200

    Add underDevelopment component

---
+++ src/components/UnderDevelopment/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { MdOutlineConstruction as InDevelopmentIcon } from 'react-icons/md'
+
+import styles from './UnderDevelopment.module.scss'
+import Typography from '../UI/Typography'
+import { useTranslations } from 'next-intl'
+
+const UnderDevelopment = () => {
+  const t = useTranslations('UnderDevelopement')
+  return (
+    <div className={styles.root}>
+      <InDevelopmentIcon size={22} className={styles.icon} />
+      <Typography align="left" size="m" weight="semiBold">
+        {t('title')}
+      </Typography>
+    </div>
+  )
+}
+
+export default UnderDevelopment
