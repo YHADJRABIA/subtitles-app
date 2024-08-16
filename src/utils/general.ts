dae3c5149b22ad0907e4e0b476177b695abe54b0
YHR <h_yacine@live.fr>
2024-08-16 23:51:24 +0200

    Update production link

--- src/utils/general.ts
+++ src/utils/general.ts
@@ -1,8 +1,13 @@
-export const PRODUCTION_URL = '' // TODO: complete before hosting
-
+const PRODUCTION_URL = 'https://pysubs.vercel.app'
+const STAGING_URL = 'https://staging.pysubs.vercel.app'
 const DEVELOPMENT_URL = 'http://localhost:3000'
 
 export const isProduction = process.env.NODE_ENV === 'production'
 export const isDevelopment = process.env.NODE_ENV === 'development'
+export const isStaging = process.env.NODE_ENV === 'test'
 export const isClient = typeof window !== 'undefined'
-export const websiteUrl = isProduction ? PRODUCTION_URL : DEVELOPMENT_URL
+export const websiteUrl = isProduction
+  ? PRODUCTION_URL
+  : isStaging
+    ? STAGING_URL
+    : DEVELOPMENT_URL
