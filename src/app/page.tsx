729a466679ef175044c3c59e17614c685ddceae8
YHR <h_yacine@live.fr>
2024-08-16 22:30:40 +0200

    Fix issue preventing build (Next-Intl's redirect can't be accessed outside of app\[locale]) + use next/navigation & disable linter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
-import { defaultLocale, redirect } from '@/lib/i18n/navigation'
+import { defaultLocale } from '@/lib/i18n/navigation'
+// eslint-disable-next-line no-restricted-imports
+import { redirect } from 'next/navigation' // Shouldn't call Next-Intl's redirect here (outisde of /[locale]/ folder). Fails build otherwise
 
 // Redirect '/' to '/(default-locale)'
 const IndexPage = () => {
