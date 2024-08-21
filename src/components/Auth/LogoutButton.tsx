f9899858e8c30de4780b2ee9f6a8027d14fda453
YHR <h_yacine@live.fr>
2024-08-21 14:59:33 +0200

    Prevent multiple API calls upon logout

--- src/components/Auth/LogoutButton.tsx
+++ src/components/Auth/LogoutButton.tsx
@@ -1,10 +1,11 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from '../UI/Button'
 import { handleLogout } from '@/actions/auth'
 import cn from 'classnames'
 import styles from './LogoutButton.module.scss'
 import Typography from '../UI/Typography'
+import { getErrorMessage } from '@/utils/errors'
 
 interface PropTypes {
   className?: string
@@ -12,11 +13,24 @@ interface PropTypes {
 }
 
 const LogoutButton = ({ className, label }: PropTypes) => {
+  const [isLoading, setIsLoading] = useState(false)
+
+  const onLogoutClick = async () => {
+    setIsLoading(true)
+    try {
+      await handleLogout()
+    } catch (err) {
+      console.error(getErrorMessage(err))
+    } finally {
+      setIsLoading(false)
+    }
+  }
   return (
     <Button
       className={cn(styles.root, className)}
       variation="secondary"
-      onClick={handleLogout}
+      isLoading={isLoading}
+      onClick={onLogoutClick}
     >
       <Typography tag="span" size="xs" weight="semiBold">
         {label}
