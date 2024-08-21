73b52d3b51a439360e3b553bf444bd0487c1caad
YHR <h_yacine@live.fr>
2024-08-21 20:38:51 +0200

    Add customStyle to header

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { CSSProperties } from 'react'
 import styles from './Header.module.scss'
 import Nav from './Nav'
 import Logo from './Logo'
@@ -7,11 +7,12 @@ import cn from 'classnames'
 interface PropTypes {
   isConnected: boolean
   className?: string
+  customStyle?: CSSProperties
 }
 
-const Header = ({ isConnected, className }: PropTypes) => {
+const Header = ({ isConnected, className, customStyle }: PropTypes) => {
   return (
-    <header className={cn(styles.root, className)}>
+    <header className={cn(styles.root, className)} style={customStyle}>
       <Logo isInvertedColor size={50} />
       <Nav className={styles.nav} isConnected={isConnected} />
     </header>
