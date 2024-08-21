a52a640f30eaa4078604cf9c587a31f0a377ddd2
YHR <h_yacine@live.fr>
2024-08-21 20:43:31 +0200

    Make transform fluid

--- src/components/Layout/Header/ConcealableHeader/index.tsx
+++ src/components/Layout/Header/ConcealableHeader/index.tsx
@@ -15,9 +15,12 @@ const ConcealableHeader = ({ isConnected, className }: PropTypes) => {
   const scrollDirection = useScrollDirection()
   const isDownScroll = scrollDirection === 'down'
 
+  const translateY = () => (isDownScroll ? '-100%' : 0)
+
   return (
     <Header
-      className={cn(styles.root, className, { [styles.hidden]: isDownScroll })} // Fix glitch of header disappearing instantly because of instant class removal
+      className={cn(styles.root, className)} // Fix glitch of header disappearing instantly because of instant class removal
+      customStyle={{ transform: `translateY(${translateY()})` }}
       isConnected={isConnected}
     />
   )
