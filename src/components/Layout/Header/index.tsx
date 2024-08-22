703fab38de5f9a2fae261aaf635983f56c8cb5a1
YHR <h_yacine@live.fr>
2024-08-22 12:07:37 +0200

    Replace component with prop

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,18 +1,33 @@
-import React, { CSSProperties } from 'react'
+'use client'
+import React from 'react'
 import styles from './Header.module.scss'
 import Nav from './Nav'
 import Logo from './Logo'
 import cn from 'classnames'
+import { useScrollDirection } from '@/hooks/useScrollDirection'
 
 interface PropTypes {
   isConnected: boolean
   className?: string
-  customStyle?: CSSProperties
+  isConcealable?: boolean
 }
 
-const Header = ({ isConnected, className, customStyle }: PropTypes) => {
+const Header = ({
+  isConnected,
+  className,
+  isConcealable = false,
+}: PropTypes) => {
+  const scrollDirection = useScrollDirection()
+  const isDownScroll = scrollDirection === 'down'
+  const translateY = () => (isDownScroll ? '-100%' : 0)
+
   return (
-    <header className={cn(styles.root, className)} style={customStyle}>
+    <header
+      className={cn(styles.root, className)}
+      style={
+        isConcealable ? { transform: `translateY(${translateY()})` } : undefined
+      }
+    >
       <Logo isInvertedColor size={50} />
       <Nav className={styles.nav} isConnected={isConnected} />
     </header>
