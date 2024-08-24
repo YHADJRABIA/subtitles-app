a604c16ab43df741a1eb0cdd3af31db41a1496c4
YHR <h_yacine@live.fr>
2024-08-24 23:15:41 +0200

    Edit modal's title's alignmenet

--- src/components/Modals/Modal.tsx
+++ src/components/Modals/Modal.tsx
@@ -3,7 +3,6 @@ import cn from 'classnames'
 import styles from './Modal.module.scss'
 import Typography from '@/components/UI/Typography'
 import { RiCloseLargeFill as CloseIcon } from 'react-icons/ri'
-import useIsOnMobile from '@/hooks/useIsOnMobile'
 
 interface PropTypes {
   isOpen: boolean
@@ -14,8 +13,6 @@ interface PropTypes {
 }
 
 const Modal = ({ isOpen, onClose, title, className, children }: PropTypes) => {
-  const isOnMobile = useIsOnMobile()
-
   if (!isOpen || !children) return null
 
   return (
@@ -24,13 +21,7 @@ const Modal = ({ isOpen, onClose, title, className, children }: PropTypes) => {
         <CloseIcon size={20} className={styles.closeIcon} onClick={onClose} />
 
         {title && (
-          <Typography
-            align={isOnMobile ? 'left' : 'center'}
-            tag="h2"
-            weight="bold"
-            size="xl"
-            className={styles.title}
-          >
+          <Typography tag="h2" weight="bold" size="xl" className={styles.title}>
             {title}
           </Typography>
         )}
