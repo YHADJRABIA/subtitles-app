793f5a0d0388dacc3832e8e26efc15d8c539fdc3
YHR <h_yacine@live.fr>
2024-08-25 15:02:22 +0200

    Finish up user-deletion route

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,13 +1,11 @@
 import { connectDB } from '@/lib/mongodb'
 import { NextRequest, NextResponse } from 'next/server'
-
 import { getErrorMessage, getZodErrors } from '@/utils/errors'
 import { deleteUserById, getUserById } from '@/utils/db/user'
-
 import { getLocaleFromNextRequest } from '@/utils/cookies'
-
 import { getTranslations } from 'next-intl/server'
 import { UserDeleteValidator } from '@/types/schemas/dashboard'
+import { getUserSession } from '@/utils/session'
 
 connectDB()
 
@@ -16,16 +14,13 @@ export async function DELETE(req: NextRequest) {
 
   try {
     const locale = getLocaleFromNextRequest(req)
-
     const [t_zod, t] = [
       await getTranslations({ locale, namespace: 'Zod' }),
       await getTranslations({ locale, namespace: 'User' }),
     ]
-    // Check for permission to delete user
 
+    // Validate request params
     const searchParams = UserDeleteValidator(t_zod).safeParse(rawSearchParams)
-
-    // Form validation
     if (!searchParams.success) {
       const zodErrors = getZodErrors(searchParams.error)
       return NextResponse.json(
@@ -36,23 +31,36 @@ export async function DELETE(req: NextRequest) {
 
     const id = searchParams.data
 
-    // Check if user exists under this id
+    // Check if user exists
     const existingUser = await getUserById(id)
-
-    // Nothing to delete
-    if (!existingUser)
+    if (!existingUser) {
       return NextResponse.json(
-        {
-          message: t('user_not_found'),
-          success: false,
-        },
-        {
-          status: 404,
-        }
+        { message: t('user_not_found'), success: false },
+        { status: 404 }
       )
+    }
 
-    // Delete user // TODO uncomment after finishing authorisation
-    /*     await deleteUserById(id) */
+    // Verify user's identity
+    const user = await getUserSession()
+
+    // User not authenticated
+    if (!user) {
+      return NextResponse.json(
+        { message: t('unauthorised'), success: false },
+        { status: 401 }
+      )
+    }
+
+    // Ensure only the authenticated user can delete own account
+    if (user.id !== id) {
+      return NextResponse.json(
+        { message: t('unauthorised'), success: false },
+        { status: 403 }
+      )
+    }
+
+    // Delete user
+    await deleteUserById(id)
 
     return NextResponse.json({
       message: t('user_deleted'),
