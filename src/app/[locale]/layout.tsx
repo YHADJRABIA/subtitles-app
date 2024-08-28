654cf9619859ab83dcfdad80264fac05a2aea291
YHR <h_yacine@live.fr>
2024-08-28 21:09:16 +0200

    Add og metadata

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -7,6 +7,7 @@ import { getTranslations } from 'next-intl/server'
 import { Metadata } from 'next/types'
 import { Locale } from '@/types/locale'
 import { Analytics } from '@vercel/analytics/react'
+import { websiteUrl } from '@/utils/general'
 
 const inter = Inter({ subsets: ['latin', 'cyrillic'], variable: '--font-body' })
 const literate = Literata({
@@ -28,6 +29,21 @@ export const generateMetadata = async ({
   return {
     title: `${t('prefix')} ${t('NotFound.title')}`,
     description: t('NotFound.description'),
+    openGraph: {
+      title: t('site_name'),
+      description: t('Homepage.description'),
+      url: websiteUrl,
+      siteName: t('site_name'),
+      images: [
+        {
+          url: '/logo.svg',
+          width: 800,
+          height: 600,
+        },
+      ],
+      locale,
+      type: 'website',
+    },
   }
 }
 
