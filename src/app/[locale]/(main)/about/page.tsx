83247402739071cb6b10a407a61b852b424b988b
YHR <h_yacine@live.fr>
2024-09-02 20:34:47 +0200

    Fetch metadata from CMS

--- src/app/[locale]/(main)/about/page.tsx
+++ src/app/[locale]/(main)/about/page.tsx
@@ -1,8 +1,6 @@
 import Typography from '@/components/UI/Typography'
 import styles from './page.module.scss'
 import Accordion from '@/components/Accordion'
-import { getTranslations } from 'next-intl/server'
-import { Metadata } from 'next'
 import TextWithIcon from '@/components/UI/ItemsList/TextWithIcon'
 import {
   PiTranslateThin,
@@ -15,55 +13,49 @@ import { executeQuery } from '@/lib/datocms/executeQuery'
 import { graphql } from '@/lib/datocms/graphql'
 import { draftMode } from 'next/headers'
 import { notFound } from 'next/navigation'
+import { generateMetadataFn } from '@/lib/datocms/generateMetaDataFn'
+import { TagFragment } from '@/lib/datocms/commonFragments'
 
-export const generateMetadata = async ({
-  params: { locale },
-}: MetaDataProps): Promise<Metadata> => {
-  const t = await getTranslations({ locale, namespace: 'Metadata' })
-
-  return {
-    title: `${t('prefix')} ${t('About.title')}`,
-    description: t('About.description'),
-  }
-}
-
-/**
- * The GraphQL query that will be executed for this route to generate the page
- * content and metadata.
- *
- * Thanks to gql.tada, the result will be fully typed!
- */
-const getAboutPage = graphql(`
-  query AboutPageQuery($locale: SiteLocale) {
-    aboutPage(locale: $locale, fallbackLocales: en) {
-      seo {
-        title
-        description
-      }
-
-      title
-
-      faq {
-        title
-        content {
-          question
-          answer
+const query = graphql(
+  `
+    query AboutPageQuery($locale: SiteLocale) {
+      aboutPage(locale: $locale, fallbackLocales: en) {
+        _seoMetaTags {
+          ...TagFragment
         }
-      }
 
-      highlights {
         title
-        block {
+
+        faq {
           title
-          description
-          icon
+          content {
+            question
+            answer
+          }
+        }
+
+        highlights {
+          title
+          block {
+            title
+            description
+            icon
+          }
         }
       }
     }
-  }
-`)
+  `,
+  [TagFragment]
+)
+
+export const generateMetadata = generateMetadataFn({
+  query,
+  buildQueryVariables: ({ params: { locale } }: MetaDataProps) => ({ locale }),
+  pickSeoMetaTags: data => data.aboutPage?._seoMetaTags,
+})
 
 const iconMap = {
+  // TODO: relocate somewhere better
   MonitorIcon: PiMonitorPlayThin,
   TipJarIcon: PiTipJarThin,
   TranslateIcon: PiTranslateThin,
@@ -72,7 +64,7 @@ const iconMap = {
 export default async function AboutPage({ params: { locale } }: MetaDataProps) {
   const { isEnabled: isDraftModeEnabled } = draftMode()
 
-  const { aboutPage } = await executeQuery(getAboutPage, {
+  const { aboutPage } = await executeQuery(query, {
     variables: { locale },
     includeDrafts: isDraftModeEnabled,
   })
