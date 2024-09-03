5387510f3b71cc26a94f94ef5ac4d4a1e1f44035
YHR <h_yacine@live.fr>
2024-09-03 15:48:42 +0200

    Refactor query

--- src/app/[locale]/(main)/about/page.tsx
+++ src/app/[locale]/(main)/about/page.tsx
@@ -10,46 +10,13 @@ import {
 import { Col, Row } from '@/components/UI/Grid'
 import { MetaDataProps } from '../../layout'
 import { executeQuery } from '@/lib/datocms/executeQuery'
-import { graphql } from '@/lib/datocms/graphql'
 import { draftMode } from 'next/headers'
 import { notFound } from 'next/navigation'
 import { generateMetadataFn } from '@/lib/datocms/generateMetaDataFn'
-import { TagFragment } from '@/lib/datocms/commonFragments'
-
-const query = graphql(
-  `
-    query AboutPageQuery($locale: SiteLocale) {
-      aboutPage(locale: $locale, fallbackLocales: en) {
-        _seoMetaTags {
-          ...TagFragment
-        }
-
-        title
-
-        faq {
-          title
-          content {
-            question
-            answer
-          }
-        }
-
-        highlights {
-          title
-          block {
-            title
-            description
-            icon
-          }
-        }
-      }
-    }
-  `,
-  [TagFragment]
-)
+import { aboutPageQuery } from '@/queries/aboutPage'
 
 export const generateMetadata = generateMetadataFn({
-  query,
+  query: aboutPageQuery,
   buildQueryVariables: ({ params: { locale } }: MetaDataProps) => ({ locale }),
   pickSeoMetaTags: data => data.aboutPage?._seoMetaTags,
 })
@@ -60,11 +27,12 @@ const iconMap = {
   TipJarIcon: PiTipJarThin,
   TranslateIcon: PiTranslateThin,
 }
+type IconKeys = keyof typeof iconMap
 
 export default async function AboutPage({ params: { locale } }: MetaDataProps) {
   const { isEnabled: isDraftModeEnabled } = draftMode()
 
-  const { aboutPage } = await executeQuery(query, {
+  const { aboutPage } = await executeQuery(aboutPageQuery, {
     variables: { locale },
     includeDrafts: isDraftModeEnabled,
   })
@@ -88,7 +56,7 @@ export default async function AboutPage({ params: { locale } }: MetaDataProps) {
 
       <Row className={styles.highlights} Tag="section">
         {highlights.block.map((item, idx) => {
-          const icon = iconMap[item.icon]
+          const icon = iconMap[item.icon as IconKeys]
           return (
             <Col key={idx} width={[12, 6, 4]}>
               <TextWithIcon
@@ -101,17 +69,22 @@ export default async function AboutPage({ params: { locale } }: MetaDataProps) {
         })}
       </Row>
 
-      <section>
-        <Typography tag="h2" weight="bold" className={styles.title}>
-          {faq.title}
-        </Typography>
-        <Accordion
-          items={faqItems}
-          backgroundColor="var(--primary-gray-color)"
-          expandMultiple
-          hasBackgroundEffect
-        />
-      </section>
+      {faq && (
+        <section>
+          <Typography tag="h2" weight="bold" className={styles.title}>
+            {faq.title}
+          </Typography>
+
+          {faqItems?.length && (
+            <Accordion
+              items={faqItems}
+              backgroundColor="var(--primary-gray-color)"
+              expandMultiple
+              hasBackgroundEffect
+            />
+          )}
+        </section>
+      )}
     </div>
   )
 }
