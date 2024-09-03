c7da35ab02c63a79b31b1cbe9713a4394880aca2
YHR <h_yacine@live.fr>
2024-09-03 15:45:03 +0200

    Relocate aboutpage query to separate folder

---
+++ src/queries/aboutPage.ts
@@ -0,0 +1,34 @@
+import { TagFragment } from '@/lib/datocms/commonFragments'
+import { graphql } from '@/lib/datocms/graphql'
+
+export const aboutPageQuery = graphql(
+  `
+    query AboutPageQuery($locale: SiteLocale) {
+      aboutPage(locale: $locale, fallbackLocales: en) {
+        _seoMetaTags {
+          ...TagFragment
+        }
+
+        title
+
+        faq {
+          title
+          content {
+            question
+            answer
+          }
+        }
+
+        highlights {
+          title
+          block {
+            title
+            description
+            icon
+          }
+        }
+      }
+    }
+  `,
+  [TagFragment]
+)
