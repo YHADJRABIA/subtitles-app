c2b23530b18b9d907c0bf551e1fea3d339b1fcd5
YHR <h_yacine@live.fr>
2024-09-06 22:59:13 +0200

    Add revalidation api route

---
+++ src/app/api/series/revalidate/route.ts
@@ -0,0 +1,49 @@
+import { getLocaleFromNextRequest } from '@/utils/cookies'
+import { getErrorMessage } from '@/utils/errors'
+import { getTranslations } from 'next-intl/server'
+import { revalidatePath } from 'next/cache'
+import { NextResponse, NextRequest } from 'next/server'
+
+const { NEXT_ISR_REVALIDATION_TOKEN: revalidationToken } = process.env
+
+// Called by CMS webhook when content is published/unpublished
+export async function POST(req: NextRequest) {
+  // Check for secret to validate request
+  const token = req.nextUrl.searchParams.get('token')
+  const locale = getLocaleFromNextRequest(req)
+  const t = await getTranslations({ locale, namespace: 'Revalidation' })
+
+  try {
+    if (token !== revalidationToken) {
+      return NextResponse.json(
+        { message: t('unauthorised'), success: false },
+        { status: 401 }
+      )
+    }
+
+    // Parse request body
+    const body = await req.json()
+    // Get slug of page to revalidate
+    const slug = body.slug_to_revalidate
+
+    // Ensure that request body is not empty
+    if (!body || !slug) {
+      return NextResponse.json(
+        { message: t('invalid_request'), success: false },
+        { status: 400 }
+      )
+    }
+
+    // Revalidate the page
+    revalidatePath(`/series/${slug}`)
+    return NextResponse.json({
+      message: t('successful_revalidation'),
+      success: true,
+    })
+  } catch (err) {
+    return NextResponse.json(
+      { message: getErrorMessage(err), success: false },
+      { status: 500 }
+    )
+  }
+}
