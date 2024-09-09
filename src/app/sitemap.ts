02ca83eb18ac41f6d5665cef6fc695ec1ae413b8
YHR <h_yacine@live.fr>
2024-09-09 15:26:43 +0200

    Add alternates

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,8 +3,10 @@ import { allSeriesQuery } from '@/gql/queries/allSeriesPage'
 import { executeQuery } from '@/lib/datocms/executeQuery'
 import { pathnames } from '@/lib/i18n/navigation'
 import { websiteUrl } from '@/utils/general'
-import { languages } from '@/utils/internationalisation/language'
 import { MetadataRoute } from 'next'
+
+// Note: going to /sitemap.xml shows a wrong format because of adding "alternates", the sitemap remains valid if opened in a text editor or run through validators
+// https://github.com/vercel/next.js/issues/66363
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const { allSeries } = await executeQuery(allSeriesQuery)
@@ -17,11 +19,11 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       lastModified: series.updatedAt,
       changeFrequency: 'monthly',
       priority: 0.8,
-      /* alternates: {
+      alternates: {
         languages: {
           fr: `${websiteUrl}/fr/${pathnames['/series'].fr}/${slug}`,
         },
-      },*/
+      },
     }
   })
 
@@ -31,33 +33,33 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       lastModified: '2024-08-07T15:45:15+02:00', // TODO: Update when homepage is fetched from CMS
       changeFrequency: 'monthly',
       priority: 1,
-      /*  alternates: {
+      alternates: {
         languages: {
           fr: `${websiteUrl}/fr`,
         },
-      }, */
+      },
     },
     {
       url: `${websiteUrl}${pathnames['/about'].en}`,
       lastModified: aboutPage?.updatedAt,
       changeFrequency: 'monthly',
       priority: 0.5,
-      /*   alternates: {
+      alternates: {
         languages: {
           fr: `${websiteUrl}/fr/${pathnames['/about'].fr}`,
         },
-      }, */
+      },
     },
     {
       url: `${websiteUrl}${pathnames['/series'].en}`,
       lastModified: allSeries[0].updatedAt, // TODO: Update when there are more series
       changeFrequency: 'monthly',
       priority: 0.8,
-      /*       alternates: {
+      alternates: {
         languages: {
           fr: `${websiteUrl}/fr/${pathnames['/series'].fr}`,
         },
-      }, */
+      },
     },
   ]
 
