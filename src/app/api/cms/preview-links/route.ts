a5fb5683c2088447386d7c935d60792f60243a56
YHR <h_yacine@live.fr>
2024-09-13 00:58:26 +0200

    Add route to interact with CMS preview plugin

---
+++ src/app/api/cms/preview-links/route.ts
@@ -0,0 +1,107 @@
+import { recordToWebsiteRoute } from '@/lib/datocms/recordInfo'
+import { type NextRequest, NextResponse } from 'next/server'
+
+import { getLocaleFromNextRequest } from '@/utils/cookies'
+import { getTranslations } from 'next-intl/server'
+import {
+  handleUnexpectedError,
+  invalidRequestResponse,
+  withCORS,
+} from '@/utils/datoCms/api'
+
+export function OPTIONS() {
+  return new Response('OK', withCORS())
+}
+
+type PreviewLink = {
+  label: string
+  url: string
+  reloadPreviewOnRecordUpdate?: boolean | { delayInMs: number }
+}
+
+type WebPreviewsResponse = {
+  previewLinks: PreviewLink[]
+}
+
+/**
+ * This route handler implements the Previews webhook required for the "Web
+ * Previews" plugin:
+ *
+ * https://www.datocms.com/marketplace/plugins/i/datocms-plugin-web-previews#the-previews-webhook
+ */
+
+const { SECRET_API_TOKEN: secretToken } = process.env
+
+export async function POST(req: NextRequest): Promise<NextResponse> {
+  const locale = getLocaleFromNextRequest(req)
+  const t = await getTranslations({ locale, namespace: 'CMS' })
+  try {
+    const token = req.nextUrl.searchParams.get('token')
+
+    if (token !== secretToken) {
+      return invalidRequestResponse(t('invalid_token'), 401)
+    }
+
+    /**
+     * The plugin sends the record and model for which the user wants a preview,
+     * along with information about which locale is currently viewed in
+     * the interface
+     */
+    const { item, itemType, locale } = await req.json()
+
+    // This info is needed to generate the associated frontend URL
+    const url = await recordToWebsiteRoute(
+      item,
+      itemType.attributes.api_key,
+      locale
+    )
+
+    const response: WebPreviewsResponse = { previewLinks: [] }
+
+    const status = item.meta.status
+
+    if (url) {
+      /**
+       * If status is not published, it means it has a current version different
+       * from the published one, so it's in draft version.
+       */
+      if (status !== 'published') {
+        /**
+         * Generates a URL that initially enters Next.js' draft mode, then
+         * redirects to the desired URL
+         */
+        response.previewLinks.push({
+          label: 'Draft version',
+          url: new URL(
+            /*
+             * Generate the URL in a way that it first passes through the
+             * draft-mode-enabling endpoint.
+             */
+            `/api/cms/draft-mode/enable?url=${url}&token=${token}`,
+            req.url
+          ).toString(),
+        })
+      }
+
+      /** If status is not draft, it means it has a published version. */
+      if (status !== 'draft') {
+        /**
+         * Generate a URL that first exits from Next.js' draft mode, then
+         * redirects to the desired URL.
+         */
+        response.previewLinks.push({
+          label: 'Published version',
+          url: new URL(
+            `/api/cms/draft-mode/disable?url=${url}`,
+            req.url
+          ).toString(),
+        })
+      }
+    }
+
+    // Respond in the format expected by the plugin
+    return NextResponse.json(response, withCORS())
+  } catch (err) {
+    return handleUnexpectedError(err)
+  }
+}
