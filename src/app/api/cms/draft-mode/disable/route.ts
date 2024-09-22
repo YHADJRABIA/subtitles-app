f283099e6ae89756781c2fe2766f5926c5fe46cd
YHR <h_yacine@live.fr>
2024-09-22 10:58:47 +0200

    Remove eslint-disable-next-line

--- src/app/api/cms/draft-mode/disable/route.ts
+++ src/app/api/cms/draft-mode/disable/route.ts
@@ -7,7 +7,6 @@ import { getLocaleFromSearchParam } from '@/utils/request'
 import { isNonRelativeUrl } from '@/utils/string'
 import { getTranslations } from 'next-intl/server'
 import { draftMode } from 'next/headers'
-// eslint-disable-next-line no-restricted-imports
 import { redirect } from 'next/navigation' // Important to not use custom next-intl's redirect here, because api lies outside of the [...locale] folder
 
 import type { NextRequest } from 'next/server'
--- src/app/api/cms/draft-mode/enable/route.ts
+++ src/app/api/cms/draft-mode/enable/route.ts
@@ -7,7 +7,6 @@ import { draftMode } from 'next/headers'
 import type { NextRequest } from 'next/server'
 import { getTranslations } from 'next-intl/server'
 import { isNonRelativeUrl } from '@/utils/string'
-// eslint-disable-next-line no-restricted-imports
 import { redirect } from 'next/navigation' // Important to not use custom next-intl's redirect here, because api lies outside of the [...locale] folder
 import { getLocaleFromSearchParam } from '@/utils/request'
 
