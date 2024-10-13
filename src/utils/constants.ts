7d87552ce304160b98d4f6d2eabf2efb878f831d
YHR <h_yacine@live.fr>
2024-10-13 18:59:05 +0200

    Delete unused link

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,4 @@
 export const SUPPORT_LINK = 'https://buymeacoffee.com/y.h.r'
-export const PATRUL_TRAILER_LINK = 'https://www.youtube.com/watch?v=nNmZNZQ20v8'
 export const PORTFOLIO_LINK = 'https://yhr.vercel.app'
 export const GOOGLE_SEARCH_CONSOLE_VERIFICATION =
   'M6aa3CbXmeU9R8s22ny5So7N0AQo9CmsNyQpZeytuKc'
