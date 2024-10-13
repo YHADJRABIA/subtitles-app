1ffaa8d974f33b4a5868f62a22c4d9a1caa747e6
YHR <h_yacine@live.fr>
2024-10-13 19:00:40 +0200

    Update string

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 export const SUPPORT_LINK = 'https://buymeacoffee.com/y.h.r'
 export const PORTFOLIO_LINK = 'https://yhr.vercel.app'
 export const GOOGLE_SEARCH_CONSOLE_VERIFICATION =
-  'M6aa3CbXmeU9R8s22ny5So7N0AQo9CmsNyQpZeytuKc'
+  'jzLSzx6jitS6zDJe1iqedkPKJg1Lpwg'
