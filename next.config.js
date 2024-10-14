8ca1856a62c3783cc2b6813734893f8d003618ba
YHR <h_yacine@live.fr>
2024-10-14 22:45:15 +0200

    Test without headers

--- next.config.js
+++ next.config.js
@@ -40,7 +40,7 @@ const nextConfig = {
       },
     ],
   },
-  headers() {
+  /*   headers() {
     return [
       {
         source: '/(.*)',
@@ -52,7 +52,7 @@ const nextConfig = {
         ],
       },
     ]
-  },
+  }, */
   webpack(config) {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find(rule =>
