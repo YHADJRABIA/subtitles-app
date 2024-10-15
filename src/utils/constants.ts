f9b45cf7e4d63ac48c0f27a9bf04417f7353c7ba
YHR <h_yacine@live.fr>
2024-10-15 10:48:35 +0200

    Fix string

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 export const SUPPORT_LINK = 'https://buymeacoffee.com/y.h.r'
 export const PORTFOLIO_LINK = 'https://yhr.vercel.app'
 export const GOOGLE_SEARCH_CONSOLE_VERIFICATION =
-  "jzLSzx6jitS6zDJe1iqedkPKJg1Lpwg-LCds5o0umUE
+  'jzLSzx6jitS6zDJe1iqedkPKJg1Lpwg-LCds5o0umUE'
