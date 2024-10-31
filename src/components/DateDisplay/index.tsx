beaade65f267666ee43b81f0564d6af7f7dba311
YHR <h_yacine@live.fr>
2024-10-31 23:14:44 +0100

    Draft

--- src/components/DateDisplay/index.tsx
+++ src/components/DateDisplay/index.tsx
@@ -1,7 +1,7 @@
 import React, { useMemo } from 'react'
 import Typography, { TextSize } from '@/components/UI/Typography'
 import { formatDate, isToday, isYesterday } from '@/utils/date'
-import { useTranslations } from 'next-intl'
+import { useFormatter, useTranslations } from 'next-intl'
 
 interface PropTypes {
   date: string
@@ -10,22 +10,32 @@ interface PropTypes {
 }
 
 const DateDisplay = ({ date, showTime = false, size = 'xxs' }: PropTypes) => {
-  const t = useTranslations('DateDisplay')
-  const formattedDate = formatDate(date, { showTime })
+  /*   const t = useTranslations('DateDisplay')
+  const formattedDate = formatDate(date, { showTime }) */
+
+  const format = useFormatter()
+
+  const dateTime = new Date(date)
+
+  const timeAgo = format.relativeTime(dateTime)
+  const timeDate = format.dateTime(dateTime, {
+    hour: 'numeric',
+    minute: 'numeric',
+  })
 
   // Simplified useMemo to get dateValue
-  const dateValue = useMemo(() => {
+  /*   const dateValue = useMemo(() => {
     if (isToday(date)) return t('today')
     if (isYesterday(date)) return t('yesterday')
     return typeof formattedDate === 'object' ? formattedDate.date : undefined
   }, [date, formattedDate, t])
 
   const timeValue =
-    typeof formattedDate === 'object' && showTime ? formattedDate.time : null
+    typeof formattedDate === 'object' && showTime ? formattedDate.time : null */
 
   return (
     <Typography size={size} weight="semiLight">
-      {t('at', { date: dateValue, time: timeValue })}
+      {timeDate}
     </Typography>
   )
 }
