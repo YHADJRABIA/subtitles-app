1f128fb48ee2058fdaea0f10a1c01bbfcf560b5c
YHR <h_yacine@live.fr>
2024-11-07 00:24:31 +0100

    Add PUT method to update user fields

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -12,6 +12,67 @@ import { getUserSession } from '@/utils/session'
 
 connectDB()
 
+export async function PUT(req: NextRequest) {
+  try {
+    const locale = getLocaleFromNextRequest(req)
+
+    const [t_zod, t] = [
+      await getTranslations({ locale, namespace: 'Zod' }),
+      await getTranslations({ locale, namespace: 'User' }),
+    ]
+
+    const rawBody = await req.json()
+
+    const body = UserUpdateValidator(t_zod as any).safeParse(rawBody)
+
+    if (!body.success) {
+      const zodErrors = getZodErrors(body.error)
+      return NextResponse.json(
+        { message: zodErrors.message, success: false },
+        { status: 400 }
+      )
+    }
+
+    const { id, user } = body.data
+
+    // Check if user exists
+    const existingUser = await getUserById(id)
+    if (!existingUser) {
+      return NextResponse.json(
+        { message: t('user_not_found'), success: false },
+        { status: 404 }
+      )
+    }
+
+    // Verify user's identity
+    const currentUser = await getUserSession()
+    if (!currentUser) {
+      return NextResponse.json(
+        { message: t('unauthorised'), success: false },
+        { status: 401 }
+      )
+    }
+
+    // Ensure only authenticated users may update their own name
+    if (currentUser.id !== id) {
+      return NextResponse.json(
+        { message: t('unauthorised'), success: false },
+        { status: 403 }
+      )
+    }
+
+    // Update user's name
+    await updateNameById(id, user.name) // TODO: update to include more fields
+
+    return NextResponse.json({
+      message: t('user_updated'),
+      success: true,
+    })
+  } catch (error) {
+    return NextResponse.json({ error: getErrorMessage(error) }, { status: 500 })
+  }
+}
+
 export async function DELETE(req: NextRequest) {
   const rawSearchParams = req.nextUrl.searchParams.get('id') ?? ''
 
