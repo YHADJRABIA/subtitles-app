c80501da3ac329274d2926e7f00c94b5827bad0a
YHR <h_yacine@live.fr>
2024-11-11 00:40:06 +0100

    Improve EditableField UX/UI

--- src/app/[locale]/(protected)/dashboard/_components/EditableField/index.tsx
+++ src/app/[locale]/(protected)/dashboard/_components/EditableField/index.tsx
@@ -1,105 +1,110 @@
 'use client'
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import cn from 'classnames'
 import styles from './EditableField.module.scss'
 import Typography from '@/components/UI/Typography'
-/* import {
+import Separator from '@/components/Separator'
+import {
   MdOutlineEdit as PenIcon,
   MdAdd as AddIcon,
   MdSave as SaveIcon,
   MdCancel as CancelIcon,
 } from 'react-icons/md'
-import { useTranslations } from 'next-intl' */
-import Separator from '@/components/Separator'
-/* import { Button } from '@/components/UI/Button' */
+import { Button } from '@/components/UI/Button'
+import { useTranslations } from 'next-intl'
+import { getErrorMessage } from '@/utils/errors'
 
 interface PropTypes {
   label?: string
   value: string
-  /*   onValidate: (newValue: string) => void */
+  onEdit: (newValue: string) => Promise<void>
   className?: string
 }
 
-// TODO: finish development
-const EditableField = ({
-  className,
-  label,
-  value /* onValidate */,
-}: PropTypes) => {
-  /*   const t = useTranslations('EditableField')
+const EditableField = ({ className, label, value, onEdit }: PropTypes) => {
+  const t = useTranslations('EditableField')
+  const [isEditing, setIsEditing] = useState(false)
+  const [inputValue, setInputValue] = useState(value)
+  const [isSaving, setIsSaving] = useState(false)
+
   const hasValue = !!value.length
   const CtaIcon = hasValue ? PenIcon : AddIcon
 
-  const [isEditing, setIsEditing] = useState(false)
-  const [inputValue, setInputValue] = useState(value)
-
-  const handleIconClick = () => {
+  const handleEdit = () => {
     setIsEditing(true)
   }
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value)
   }
 
-  const handleSave = () => {
+  const handleSave = async () => {
     if (inputValue !== value) {
-      onValidate(inputValue)
+      setIsSaving(true)
+      try {
+        await onEdit(inputValue) // Call the passed update function
+        setIsSaving(false)
+        setIsEditing(false)
+      } catch (err) {
+        console.error('Saving EditableField failed:', getErrorMessage(err))
+        setIsSaving(false)
+      }
+    } else {
+      setIsEditing(false)
     }
-    setIsEditing(false)
   }
 
   const handleCancel = () => {
     setInputValue(value) // Revert to the original value
     setIsEditing(false)
-  } */
+  }
 
   return (
     <div className={cn(styles.root, className)}>
-      <Typography className={styles.label} size="s" weight="semiBold">
-        {label}
-      </Typography>
+      <div className={styles.heading}>
+        <Typography className={styles.label} size="s" weight="semiBold">
+          {label}
+        </Typography>
+        <Typography
+          aria-label={t(isEditing ? 'cancel' : hasValue ? 'edit' : 'add')}
+          className={styles.cta}
+          size="xxs"
+          title={t(isEditing ? 'cancel' : hasValue ? 'edit' : 'add')}
+          onClick={isEditing ? handleCancel : handleEdit}
+        >
+          {t(isEditing ? 'cancel' : hasValue ? 'edit' : 'add')}
+        </Typography>
+      </div>
+
       {/*       {isEditing ? (
         <div className={styles.editingContainer}>
           <input
+            autoFocus
+            aria-label={t('edit')}
+            className={styles.input}
             type="text"
             value={inputValue}
             onChange={handleInputChange}
-            className={styles.input}
-            autoFocus
-            aria-label={t('editField')}
           />
-
           <div className={styles.ctaSection}>
             <Button
-              onClick={handleSave}
-              className={styles.saveButton}
               aria-label={t('save')}
+              className={styles.saveButton}
+              disabled={isSaving}
+              isLoading={isSaving}
+              size="xxs"
+              onClick={handleSave}
             >
               {t('save')}
             </Button>
-            <Button
-              onClick={handleCancel}
-              className={styles.cancelButton}
-              aria-label={t('cancel')}
-            >
-              {t('cancel')}
-            </Button>
           </div>
         </div>
-      ) : ( */}
-      <Typography className={styles.value} size="s">
-        {value}
-      </Typography>
-      {/*   )}
-      {!isEditing && (
-        <CtaIcon
-          size={18}
-          className={styles.ctaIcon}
-          title={t(hasValue ? 'edit' : 'add')}
-          onClick={handleIconClick}
-          aria-label={t(hasValue ? 'edit' : 'add')}
-        />
-      )} */}
+      ) : (
+        <Typography className={styles.value} size="s">
+          {value}
+        </Typography>
+      )}
+ */}
       <Separator className={styles.separator} />
     </div>
   )
