d262104c45cb125108e6701a6894d5cc0c11f0e4
YHR <h_yacine@live.fr>
2024-11-28 11:33:29 +0100

    Add code util

---
+++ src/lib/auth/code.ts
@@ -0,0 +1,31 @@
+import { VerificationCodeModel } from '@/models/verificationCode.model'
+import {
+  deleteVerificationCodeById,
+  getVerificationCodeByEmail,
+} from '@/utils/db/verification-code'
+import { generateDigitCode } from '@/utils/random'
+
+const { EMAIL_VERIFICATION_CODE_LIFETIME_HOURS } = process.env
+
+export const generateVerificationCode = async (email: string) => {
+  // Generate random code
+  const { code, expirationDate } = generateDigitCode(
+    Number(EMAIL_VERIFICATION_CODE_LIFETIME_HOURS)
+  )
+
+  // Check if existing code already sent for this email to delete it
+  const existingCode = await getVerificationCodeByEmail(email)
+
+  if (existingCode) {
+    await deleteVerificationCodeById(existingCode.id)
+  }
+
+  // Create verification code
+  const verificationCode = await VerificationCodeModel.create({
+    email,
+    code,
+    expires: new Date(expirationDate),
+  })
+
+  return verificationCode
+}
