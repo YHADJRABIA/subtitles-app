01a4ad8f57e417343b2f1b16854fdc1f629d48ea
YHR <h_yacine@live.fr>
2024-12-05 00:10:56 +0100

    Link VerificationCode to user id

--- src/models/verificationCode.model.ts
+++ src/models/verificationCode.model.ts
@@ -2,6 +2,11 @@ import mongoose from 'mongoose'
 const { Schema } = mongoose
 
 const verificationCodeSchema = new Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
   email: {
     type: String,
     required: [true, 'Please provide an email'],
