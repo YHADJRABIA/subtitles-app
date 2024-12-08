45527e438e81b2bdf31e16896892a137fb379a95
YHR <h_yacine@live.fr>
2024-12-09 00:03:43 +0100

    Add MultiDigitInput

---
+++ src/components/MultiDigitInput/index.tsx
@@ -0,0 +1,82 @@
+import React, { KeyboardEvent, useRef } from 'react'
+import styles from './MultiDigitInput.module.scss'
+import cn from 'classnames'
+
+interface MultiDigitInputProps {
+  n: number
+  value: string
+  onChange: (value: string) => void
+  isDisabled?: boolean
+  autoFocus?: boolean
+  hasError?: boolean
+  className?: string
+}
+
+const MultiDigitInput = ({
+  n,
+  value,
+  onChange,
+  isDisabled = false,
+  autoFocus = true,
+  hasError = false,
+  className,
+}: MultiDigitInputProps) => {
+  const inputRefs = useRef<(HTMLInputElement | null)[]>([])
+
+  // Update specific input field
+  const handleInputChange = (inputValue: string, idx: number) => {
+    // Allow only single-character numeric input
+    if (inputValue.length > 1) return
+
+    // Create a multi-digit array
+    const otpArray = value.split('')
+    otpArray[idx] = inputValue // Update current index digit
+    const newValue = otpArray.join('') // Rebuild string
+
+    onChange(newValue) // Update parent value
+
+    // Move focus to next field if input is valid
+    if (inputValue && idx < n - 1) {
+      inputRefs.current[idx + 1]?.focus()
+    }
+  }
+
+  // Handle keyboard navigation
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>, idx: number) => {
+    if (e.key === 'Backspace' && idx > 0 && !value[idx]) {
+      inputRefs.current[idx - 1]?.focus() // Move focus to previous field on backspace
+    } else if (e.key === 'ArrowLeft' && idx > 0) {
+      inputRefs.current[idx - 1]?.focus() // Move focus to previous field on left arrow
+    } else if (e.key === 'ArrowRight' && idx < n - 1) {
+      if (value[idx]) {
+        inputRefs.current[idx + 1]?.focus() // Move focus to next field if current field is filled
+      } else {
+        e.preventDefault() // Prevent navigation if the current field is empty
+      }
+    }
+  }
+
+  return (
+    <div className={cn(styles.root, className)}>
+      {Array.from({ length: n }).map((_, index) => (
+        <input
+          autoFocus={autoFocus && index === 0}
+          className={cn(styles.input, { [styles.error]: hasError })}
+          disabled={isDisabled}
+          inputMode="numeric"
+          key={index}
+          maxLength={1}
+          ref={el => {
+            inputRefs.current[index] = el
+          }}
+          type="text"
+          value={value[index]} // Render  correct digit
+          onChange={e => handleInputChange(e.target.value, index)}
+          onKeyDown={e => handleKeyDown(e, index)}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default MultiDigitInput
