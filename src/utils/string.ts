5e91fb5125fff09d4eee1f5de48f34c0a974e919
YHR <h_yacine@live.fr>
2024-12-14 01:03:53 +0100

    Add string util

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -6,3 +6,28 @@ export const capitaliseFirstLetter = (string: string) => {
 export const isNonRelativeUrl = (url: string) => {
   return url.startsWith('http://') || url.startsWith('https://')
 }
+
+/**
+ * Truncate email address to specified length, ensuring the domain is preserved.
+ *
+ * @param email - Email address to truncate.
+ * @param maxLength - Max allowed length for email address.
+ * @returns Truncated email address.
+ */
+export function truncateEmail(email: string, maxLength: number): string {
+  if (email.length <= maxLength) return email
+
+  const [localPart, domain] = email.split('@')
+
+  // No truncation needed or invalid email
+  if (!domain || localPart.length + domain.length + 1 <= maxLength) return email
+
+  const maxLocalLength = maxLength - domain.length - 1 // Reserve space for "@" and domain
+
+  // Typically if domain name is too long
+  if (maxLocalLength <= 0) {
+    return `${localPart.slice(0, 1)}...@${domain}` // Return a partial email
+  }
+
+  return `${localPart.slice(0, maxLocalLength)}...@${domain}`
+}
