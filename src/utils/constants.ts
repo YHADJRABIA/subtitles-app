2149d71d25924fccd0faad291b319744ca1cbb58
YHR <h_yacine@live.fr>
2024-12-16 00:40:58 +0100

    Add OTP-modal related constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,3 +2,5 @@ export const SUPPORT_LINK = 'https://buymeacoffee.com/y.h.r'
 export const PORTFOLIO_LINK = 'https://yhr.vercel.app'
 export const GOOGLE_SEARCH_CONSOLE_VERIFICATION =
   'jzLSzx6jitS6zDJe1iqedkPKJg1Lpwg-LCds5o0umUE'
+
+export const EMAIL_UPDATE_OTP = { n: 4, expirationInMinutes: 15 }
