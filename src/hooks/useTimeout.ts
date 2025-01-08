ec9fcb7334a717d20c2ba4386a003ea041c643b2
YHR <h_yacine@live.fr>
2025-01-08 01:24:02 +0100

    Add timeout hook

---
+++ src/hooks/useTimeout.ts
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from 'react'
+import { useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect'
+
+export const useTimeout = (
+  callback: () => void,
+  delay: number | null
+): void => {
+  const savedCallback = useRef(callback)
+
+  // Remember the latest callback if it changes
+  useIsomorphicLayoutEffect(() => {
+    savedCallback.current = callback
+  }, [callback])
+
+  useEffect(() => {
+    if (delay === null) return // Exit if delay is null
+
+    const id = setTimeout(() => savedCallback.current(), delay)
+
+    return () => clearTimeout(id) // Clean up timeout on unmount or delay change
+  }, [delay])
+}
