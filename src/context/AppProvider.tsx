f0cfea580bf83a5ae6c96419c5cedec6f8585c66
YHR <h_yacine@live.fr>
2025-01-08 01:34:24 +0100

    Switch arborescence of Toast & Modal providers

--- src/context/AppProvider.tsx
+++ src/context/AppProvider.tsx
@@ -1,11 +1,11 @@
 import { ReactNode } from 'react'
 import { NextIntlClientProvider } from 'next-intl'
 import { getMessages } from 'next-intl/server'
-import { ToastContainer } from 'react-toastify'
 import AuthProvider from './AuthProvider'
 import { ModalProvider } from './ModalProvider'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/lib/auth/auth.config'
+import ToastProvider from './ToastProvider'
 
 interface PropTypes {
   children: ReactNode
@@ -21,10 +21,9 @@ const AppProvider = async ({ children }: PropTypes) => {
   return (
     <NextIntlClientProvider messages={messages}>
       <AuthProvider session={session}>
-        <ModalProvider>
-          <ToastContainer />
-          {children}
-        </ModalProvider>
+        <ToastProvider>
+          <ModalProvider>{children}</ModalProvider>
+        </ToastProvider>
       </AuthProvider>
     </NextIntlClientProvider>
   )
