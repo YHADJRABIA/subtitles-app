5d052d03939cccc8c7ae2da48146e0c1f3814ca8
YHR <h_yacine@live.fr>
2025-01-15 00:00:49 +0100

    Type localStorage keys

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,8 +1,10 @@
-export const setLocalStorageItem = <T>(key: string, value: T) => {
+import { LocalStorageKey } from '@/types/localeStorage'
+
+export const setLocalStorageItem = <T>(key: LocalStorageKey, value: T) => {
   localStorage.setItem(key, JSON.stringify(value))
 }
 
-export const getLocalStorageItem = (key: string) => {
+export const getLocalStorageItem = (key: LocalStorageKey) => {
   const stored = localStorage.getItem(key)
   if (!stored) return null
 
