c680ef2bab625660b13b27c8c5f7d7ed5cddcad3
YHR <h_yacine@live.fr>
2025-01-24 20:19:48 +0100

    Format

--- src/app/api/users/password/reset/route.ts
+++ src/app/api/users/password/reset/route.ts
@@ -22,10 +22,7 @@ export async function POST(req: NextRequest) {
 
     const [t_zod, t] = [
       await getTranslations({ locale, namespace: 'Zod' }),
-      await getTranslations({
-        locale,
-        namespace: 'Auth.PasswordReset',
-      }),
+      await getTranslations({ locale, namespace: 'Auth.PasswordReset' }),
     ]
 
     const rawBody = await req.json()
