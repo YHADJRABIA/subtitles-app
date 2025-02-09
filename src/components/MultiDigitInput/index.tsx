3caebe442324c7e297e9d2026c7ea0487adb2179
YHR <h_yacine@live.fr>
2025-02-09 20:21:44 +0100

    Decouple component from parent & store digit state locally

--- src/components/MultiDigitInput/index.tsx
+++ src/components/MultiDigitInput/index.tsx
@@ -1,10 +1,9 @@
-import React, { KeyboardEvent, useRef } from 'react'
+import React, { KeyboardEvent, useRef, useState } from 'react'
 import styles from './MultiDigitInput.module.scss'
 import cn from 'classnames'
 
 interface MultiDigitInputProps {
   n: number
-  value: string
   onChange: (value: string) => void
   isDisabled?: boolean
   autoFocus?: boolean
@@ -16,7 +15,6 @@ interface MultiDigitInputProps {
 
 const MultiDigitInput = ({
   n,
-  value,
   onChange,
   isDisabled = false,
   autoFocus = true,
@@ -25,41 +23,33 @@ const MultiDigitInput = ({
   ariaLabel,
   testId,
 }: MultiDigitInputProps) => {
+  const [digits, setDigits] = useState<string[]>(Array(n).fill(''))
   const inputRefs = useRef<(HTMLInputElement | null)[]>([])
 
   const focusInput = (idx: number) => inputRefs.current[idx]?.focus()
 
-  // Update specific input field
   const handleInputChange = (inputValue: string, idx: number) => {
     // Allow only single-character inputs
     if (inputValue.length > 1) return
 
-    // Create a multi-digit array
-    const otpArray = value.split('')
-    otpArray[idx] = inputValue // Update current index digit
-    const newValue = otpArray.join('') // Rebuild string
-
-    onChange(newValue) // Update parent value
+    const array = [...digits]
+    array[idx] = inputValue
+    setDigits(array)
+    onChange(array.join(''))
 
     const isLastField = idx === n - 1
-    if (!isLastField) focusInput(idx + 1) // Focus next field
+    if (inputValue && !isLastField) focusInput(idx + 1) // Focus next field
   }
 
-  // Handle keyboard navigation
   const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>, idx: number) => {
     const [isFirstField, isLastField] = [idx === 0, idx === n - 1]
-    const isEmptyField = !value[idx]
-
-    if (e.key === 'Backspace' && !isFirstField && isEmptyField) {
-      focusInput(idx - 1) // Move focus to previous field on backspace
+    const isEmptyField = !digits[idx]
+    if (e.key === 'Backspace' && isEmptyField && !isFirstField) {
+      focusInput(idx - 1)
     } else if (e.key === 'ArrowLeft' && !isFirstField) {
-      focusInput(idx - 1) // Move focus to previous field on left arrow
+      focusInput(idx - 1)
     } else if (e.key === 'ArrowRight' && !isLastField) {
-      if (value[idx]) {
-        focusInput(idx + 1) // Move focus to next field if current field is filled
-      } else {
-        e.preventDefault() // Prevent navigation if the current field is empty
-      }
+      focusInput(idx + 1)
     } else if (isNaN(Number(e.key))) {
       e.preventDefault() // Prevent non-numeric input
     }
@@ -71,7 +61,7 @@ const MultiDigitInput = ({
       className={cn(styles.root, className)}
       data-testid={testId}
     >
-      {Array.from({ length: n }).map((_, idx) => (
+      {digits.map((digit, idx) => (
         <input
           autoFocus={autoFocus && idx === 0}
           className={cn(styles.input, { [styles.error]: hasError })}
@@ -83,7 +73,7 @@ const MultiDigitInput = ({
             inputRefs.current[idx] = el
           }}
           type="number"
-          value={value[idx]} // Render correct digit
+          value={digit}
           onChange={e => handleInputChange(e.target.value, idx)}
           onKeyDown={e => handleKeyDown(e, idx)}
         />
