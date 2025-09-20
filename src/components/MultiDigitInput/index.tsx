9b093e16df30fc7411ec2e17dba73a6a08d0d10d
YHR <h_yacine@live.fr>
2025-09-21 00:47:16 +0200

    Fix error, input's value cannot be null

--- src/components/MultiDigitInput/index.tsx
+++ src/components/MultiDigitInput/index.tsx
@@ -23,7 +23,7 @@ const MultiDigitInput = ({
   ariaLabel,
   testId,
 }: MultiDigitInputProps) => {
-  const [digits, setDigits] = useState(Array(n).fill(null))
+  const [digits, setDigits] = useState(Array(n).fill(''))
   const inputRefs = useRef<(HTMLInputElement | null)[]>([])
 
   const focusInput = (idx: number) => inputRefs.current[idx]?.focus()
