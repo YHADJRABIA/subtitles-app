ea62b10039b9dd14c418d6a9aaa32aac17772ad6
YHR <h_yacine@live.fr>
2025-09-27 18:15:53 +0200

    Better handle error

--- src/app/[locale]/(protected)/dashboard/_components/TwoFactorAuth/index.tsx
+++ src/app/[locale]/(protected)/dashboard/_components/TwoFactorAuth/index.tsx
@@ -1,11 +1,13 @@
 'use client'
 import Typography from '@/components/UI/Typography'
 import styles from './TwoFactorAuth.module.scss'
-import React from 'react'
+import React, { useTransition } from 'react'
 import SwitchButton from '@/components/SwitchButton'
 import { useSession } from 'next-auth/react'
 import { handleToggleTwoFactorAuth } from '@/actions/user'
+import { notify } from '@/lib/toastify'
 import { useTranslations } from 'next-intl'
+import { getErrorMessage } from '@/utils/errors'
 
 interface PropTypes {
   isActive: boolean
@@ -14,14 +16,23 @@ interface PropTypes {
 const TwoFactorAuth = ({ isActive }: PropTypes) => {
   const t = useTranslations('Dashboard.Settings')
   const { update } = useSession()
+  const [isPending, startTransition] = useTransition()
 
   const handleUpdateSession = async (isOn: boolean) => {
     await update({ isTwoFactorEnabled: isOn })
   }
 
   const handleToggle = (isOn: boolean) => {
-    handleToggleTwoFactorAuth(isOn)
-    handleUpdateSession(isOn)
+    startTransition(async () => {
+      try {
+        const res = await handleToggleTwoFactorAuth(isOn)
+        await handleUpdateSession(isOn)
+        notify('success', res.data?.message ?? '')
+      } catch (err) {
+        notify('error', await getErrorMessage(err))
+        console.error('TwoFactorAuth toggle error: ', getErrorMessage(err))
+      }
+    })
   }
 
   return (
@@ -29,7 +40,11 @@ const TwoFactorAuth = ({ isActive }: PropTypes) => {
       <Typography size="xs" weight="semiBold">
         {t('two_factor_auth')}
       </Typography>
-      <SwitchButton isActive={isActive} onToggle={handleToggle} />
+      <SwitchButton
+        isActive={isActive}
+        isDisabled={isPending}
+        onToggle={handleToggle}
+      />
     </div>
   )
 }
