334e3084f98aceff8e019dd203426825f0feee2e
YHR <h_yacine@live.fr>
2025-09-28 01:53:40 +0200

    Add sendTwoFactorOTP util

--- src/lib/auth/twoFactorAuth.ts
+++ src/lib/auth/twoFactorAuth.ts
@@ -1,6 +1,28 @@
 import { updateTwoFactorAuthByUserId } from '@/utils/db/user'
 import { getErrorMessage } from '@/utils/errors'
 import { getTranslations } from 'next-intl/server'
+import { generateVerificationCode } from './code'
+import { sendTwoFactorOTPEmail } from '../mail'
+
+export const sendTwoFactorOTP = async (
+  userId: string,
+  email: string,
+  locale: string
+) => {
+  try {
+    const verificationCode = await generateVerificationCode(email, userId)
+
+    await sendTwoFactorOTPEmail(locale, email, verificationCode.code)
+
+    return {
+      data: { message: 'OTP code sent to your email', success: true },
+      status: 200,
+    }
+  } catch (err) {
+    console.error('Error in sendTwoFactorOTP: ', getErrorMessage(err))
+    return { data: null, error: getErrorMessage(err), status: 500 }
+  }
+}
 
 export const toggleTwoFactorAuth = async (
   userId: string,
