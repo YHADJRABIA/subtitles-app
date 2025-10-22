525aeb1fa0b613fd272a217eba8055233a062905
YHR <h_yacine@live.fr>
2025-10-22 15:17:17 +0200

    Add manifest

---
+++ src/app/manifest.ts
@@ -0,0 +1,44 @@
+import { MetadataRoute } from 'next'
+import { websiteUrl } from '@/utils/general'
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'PySubs',
+    short_name: 'PySubs',
+    start_url: '/',
+    scope: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#07004d',
+    description: 'Subtitles for animated series including Patrul and more.',
+    icons: [
+      {
+        src: '/favicon-16x16.png',
+        sizes: '16x16',
+        type: 'image/png',
+      },
+      {
+        src: '/favicon-32x32.png',
+        sizes: '32x32',
+        type: 'image/png',
+      },
+      {
+        src: '/favicon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: '/favicon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: '/apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+      },
+    ],
+    id: websiteUrl,
+  }
+}
