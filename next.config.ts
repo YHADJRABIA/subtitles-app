a4a77a69f31a1c2018097610bef295d2d642b30d
YHR <h_yacine@live.fr>
2025-11-01 16:57:41 +0100

    Type next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-const createNextIntlPlugin = require('next-intl/plugin')
-const path = require('path')
+import path from 'path';
+import createNextIntlPlugin from 'next-intl/plugin';
+import type { NextConfig } from 'next';
 
-const withNextIntl = createNextIntlPlugin()
+const withNextIntl = createNextIntlPlugin();
 
 // TODO: complete default-src to better secure CORS policy
 /*
@@ -19,11 +20,9 @@ const cspHeader = `
     form-action 'self';
     frame-ancestors 'self' https://pysubs.admin.datocms.com/ https://plugins-cdn.datocms.com;
     upgrade-insecure-requests;
-`
+`;
 
-/** @type {import('next').NextConfig} */
-
-const nextConfig = {
+const nextConfig: NextConfig = {
   reactStrictMode: true, // Development mode only – highlights potential problems.
   sassOptions: {
     includePaths: [path.join(__dirname, 'src/styles')],
@@ -40,7 +39,7 @@ const nextConfig = {
       },
     ],
   },
-  headers() {
+  async headers() {
     return [
       {
         source: '/(.*)',
@@ -51,35 +50,37 @@ const nextConfig = {
           },
         ],
       },
-    ]
+    ];
   },
   webpack(config) {
     // Grab the existing rule that handles SVG imports
-    const fileLoaderRule = config.module.rules.find(rule =>
-      rule.test?.test?.('.svg')
-    )
+    const fileLoaderRule = config.module?.rules.find(
+      (rule: any) => rule.test?.test?.('.svg')
+    );
 
-    config.module.rules.push(
-      // Reapply the existing rule, but only for svg imports ending in ?url
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/, // *.svg?url
-      },
-      // Convert all other *.svg imports to React components
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
-        use: ['@svgr/webpack'],
-      }
-    )
+    if (fileLoaderRule) {
+      config.module?.rules.push(
+        // Reapply the existing rule, but only for svg imports ending in ?url
+        {
+          ...fileLoaderRule,
+          test: /\.svg$/i,
+          resourceQuery: /url/, // *.svg?url
+        },
+        // Convert all other *.svg imports to React components
+        {
+          test: /\.svg$/i,
+          issuer: fileLoaderRule.issuer,
+          resourceQuery: { not: [...(fileLoaderRule.resourceQuery?.not || []), /url/] }, // exclude if *.svg?url
+          use: ['@svgr/webpack'],
+        }
+      );
 
-    // Modify the file loader rule to ignore *.svg, since we have it handled now.
-    fileLoaderRule.exclude = /\.svg$/i
+      // Modify the file loader rule to ignore *.svg, since we have it handled now.
+      fileLoaderRule.exclude = /\.svg$/i;
+    }
 
-    return config
+    return config;
   },
-}
+};
 
-module.exports = withNextIntl(nextConfig)
+export default withNextIntl(nextConfig);
