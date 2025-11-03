0771a2579877133a91d39b47ca0d9767ef86ea7e
YHR <h_yacine@live.fr>
2025-11-03 21:22:22 +0100

    Add params to breadcrumbs

--- src/hooks/useBreadcrumbs.ts
+++ src/hooks/useBreadcrumbs.ts
@@ -2,12 +2,14 @@ import { usePathname } from '@/i18n/routing'
 import { useTranslations } from 'next-intl'
 import { generateBreadcrumbs } from '@/utils/paths'
 import type { BreadcrumbItem } from '@/components/UI/Breadcrumbs'
+import { useParams } from 'next/navigation'
 
 const useBreadcrumbs = (): BreadcrumbItem[] => {
   const pathname = usePathname()
   const t = useTranslations('Breadcrumbs')
+  const params = useParams()
 
-  return generateBreadcrumbs(pathname, t)
+  return generateBreadcrumbs(pathname, t, params)
 }
 
 export default useBreadcrumbs
